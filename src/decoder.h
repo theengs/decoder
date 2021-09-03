dfdcef59da2dee73cab81314ffcf89e25d6c788c
h2zero <powellperalta@gmail.com>
2021-09-02 18:11:41 -0600

    Fix MiScale and set success on static_value.

--- src/decoder.h
+++ src/decoder.h
@@ -1,6 +1,6 @@
 #ifndef _DECODER_H_
 #define _DECODER_H_
 #include "ArduinoJson.h"
-//#define DEBUG_DECODER
+#define DEBUG_DECODER
 extern "C" bool decodeBLEJson(JsonObject& jsondata); // extern "C" needed for platformio linker?
 #endif
