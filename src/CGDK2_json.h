a99d3ac22e764dbdc40787cd3158159af10ea682
h2zero <32826625+h2zero@users.noreply.github.com>
2021-09-11 13:31:26 -0600

    Minify Json files to save space. (#4)

--- src/CGDK2_json.h
+++ src/CGDK2_json.h
@@ -1,4 +1,5 @@
-const char* _CGDK2_json = R""""(
+const char* _CGDK2_json = "{\"brand\":\"Qingping\",\"model\":\"TH lite\",\"model_id\":\"CGDK2\",\"condition\":[\"servicedata\",\"index\",0,\"8810\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,4,true,false],\"post_proc\":[\"/\",10]}}}";
+/*R""""(
 {
    "brand":"Qingping",
    "model":"TH lite",
@@ -7,11 +8,11 @@ const char* _CGDK2_json = R""""(
    "properties":{
       "tempc":{
          "decoder":["value_from_hex_data", "servicedata", 20, 4, true],
-         "post_proc":['/', 10]
+         "post_proc":["/", 10]
       },
       "hum":{
          "decoder":["value_from_hex_data", "servicedata", 24, 4, true, false],
-         "post_proc":['/', 10]
+         "post_proc":["/", 10]
       }
    }
-})"""";
+})"""";*/
--- src/LYWSD03MMC_ATC_json.h
+++ src/LYWSD03MMC_ATC_json.h
@@ -1,4 +1,5 @@
-const char* _LYWSD03MMC_ATC_json = R""""(
+const char* _LYWSD03MMC_ATC_json = "{\"brand\":\"Xiaomi\",\"model\":\"LYWSD03MMC\",\"model_id\":\"LYWSD03MMC_ATC\",\"condition\":[\"servicedata\",\"contain\",\"a4c138\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",12,4,false],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",16,2,false]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",18,2,false]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,false],\"post_proc\":[\"/\",1000]}}}";
+/* R""""(
 {
    "brand":"Xiaomi",
    "model":"LYWSD03MMC",
@@ -7,7 +8,7 @@ const char* _LYWSD03MMC_ATC_json = R""""(
    "properties":{
       "tempc":{
          "decoder":["value_from_hex_data", "servicedata", 12, 4, false],
-         "post_proc":['/', 10]
+         "post_proc":["/", 10]
       },
       "hum":{
          "decoder":["value_from_hex_data", "servicedata", 16, 2, false]
@@ -17,7 +18,7 @@ const char* _LYWSD03MMC_ATC_json = R""""(
       },
       "volt":{
          "decoder":["value_from_hex_data", "servicedata", 20, 4, false],
-         "post_proc":['/', 1000]
+         "post_proc":["/", 1000]
       }
    }
-})"""";
+})"""";*/
