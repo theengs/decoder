1a31425777be3b745ebf4e27d181c1f758eaf8b2
Florian <1technophile@users.noreply.github.com>
2021-09-11 14:04:51 -0500

    Correct WS02 delimiter position

--- src/WS02_json.h
+++ src/WS02_json.h
@@ -6,15 +6,15 @@ const char* _WS02_json = R""""(
    "condition":["manufacturerdata", "contain", "100000001a11"],
    "properties":{
       "tempc":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 20, 4, true],
+         "decoder":["value_from_hex_data", "manufacturerdata", 24, 4, true],
          "post_proc":['/', 16]
       },
       "hum":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 24, 2, true],
+         "decoder":["value_from_hex_data", "manufacturerdata", 28, 2, true],
          "post_proc":['/', 16]
       },
       "volt":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 16, 4, true],
+         "decoder":["value_from_hex_data", "manufacturerdata", 20, 4, true],
          "post_proc":['/', 1000]
       }
    }
