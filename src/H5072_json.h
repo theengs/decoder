11d6225bbdccdaf313f55c1d5aea6c88357175d6
Florian <1technophile@users.noreply.github.com>
2021-09-17 13:23:07 -0500

    Add devices (#8)
    * Add devices
    
    Co-authored-by: h2zero <32826625+h2zero@users.noreply.github.com>

---
+++ src/H5072_json.h
@@ -0,0 +1,22 @@
+const char* _H5072_json = "{\"brand\":\"Govee\",\"model\":\"Thermo Hygrometer\",\"model_id\":\"H5072\",\"condition\":[\"name\",\"contain\",\"GVH5072\",\"&\",\"manufacturerdata\",\"index\",0,\"88ec\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",6,6,false],\"post_proc\":[\"/\",1000,\">\",0,\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",6,6,false],\"post_proc\":[\"%\",1000,\"/\",10]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",12,2,false]}}}";
+
+/* R""""(
+{
+   "brand":"Govee",
+   "model":"Thermo Hygrometer",
+   "model_id":"H5072",
+   "condition":["name", "contain", "GVH5072","&"","manufacturerdata", "index", 0, "88ec"],
+   "properties":{
+      "tempc":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 6, 6, false],
+         "post_proc":["/", 10000]
+      },
+      "hum":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 6, 6, false],
+         "post_proc":["/", 100]
+      },
+      "batt":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 12, 2, false]
+      }
+   }
+})"""";*/
---
+++ src/MUE4094RT_json.h
@@ -0,0 +1,20 @@
+const char* _MUE4094RT_json = "{\"brand\":\"Xiaomi\",\"model\":\"MiLamp\",\"model_id\":\"MUE4094RT\",\"condition\":[\"servicedata\",\"contain\",\"4030dd\"],\"properties\":{\"pres\":{\"condition\":[\"servicedata\",0,\"4812\"],\"decoder\":[\"static_value\",\"true\"],\"is_bool\":1},\"darkness\":{\"condition\":[\"servicedata\",0,\"4812\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",8,2,true]}}}";
+/*
+R""""(
+{
+   "brand":"Xiaomi",
+   "model":"MiLamp",
+   "model_id":"MUE4094RT",
+   "condition":["servicedata", "contain", "4030dd"],
+   "properties":{
+      "pres":{
+         "condition":["servicedata", 0, "4812"],
+         "decoder":["static_value","true"],
+         "is_bool":1
+      },
+      "darkness":{
+         "condition":["servicedata", 0, "4812"],
+         "decoder":["value_from_hex_data", "servicedata", 8, 2, true]
+      }
+   }
+})"""";*/
