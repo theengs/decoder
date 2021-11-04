7e05cfb8f859810be927771946d01bd88f077119
Florian <1technophile@users.noreply.github.com>
2021-11-04 07:50:46 -0500

    Add IBT-X2

---
+++ src/devices/IBT_2X_json.h
@@ -0,0 +1,19 @@
+const char* _IBT_2X_json = "{\"brand\":\"Inkbird\",\"model\":\"iBBQ\",\"model_id\":\"IBT-2X\",\"condition\":[\"name\",\"index\",0,\"iBBQ\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",16,4,true],\"post_proc\":[\"/\",10]},\"tempc2\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,4,true],\"post_proc\":[\"/\",10]}}}";
+
+/*R""""(
+{
+   "brand":"Inkbird",
+   "model":"iBBQ",
+   "model_id":"IBT-2X",
+   "condition":["name", "index", 0, "iBBQ"],
+   "properties":{
+      "tempc":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 16, 4, true],
+         "post_proc":["/", 10]
+      },
+      "tempc2":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 20, 4, true],
+         "post_proc":["/", 10]
+      }
+   }
+})"""";*/
