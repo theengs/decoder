228823a858ca5f8f76ea3663afb5d61c9875e713
h2zero <powellperalta@gmail.com>
2021-12-04 17:15:53 -0700

    Update docs, add data length condition to devices.

--- src/devices/iNode_json.h
+++ src/devices/iNode_json.h
@@ -1,10 +1,10 @@
-const char* _iNode_json = "{\"brand\":\"iNode\",\"model\":\"Energy Meter\",\"model_id\":\"INEM\",\"condition\":[\"manufacturerdata\",\"index\",0,\"9082\"],\"properties\":{\".cal\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",16,4,true],\"post_proc\":[\"&\",16383]},\"power\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,4,true],\"post_proc\":[\"/\",\".cal\",\"*\",60000]},\"energy\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",8,4,true],\"post_proc\":[\"/\",\".cal\"]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2,true,false],\"post_proc\":[\">\",4,\"-\",2,\"*\",10]}}}";
+const char* _iNode_json = "{\"brand\":\"iNode\",\"model\":\"Energy Meter\",\"model_id\":\"INEM\",\"condition\":[\"manufacturerdata\",26,\"index\",0,\"9082\"],\"properties\":{\".cal\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",16,4,true],\"post_proc\":[\"&\",16383]},\"power\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,4,true],\"post_proc\":[\"/\",\".cal\",\"*\",60000]},\"energy\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",8,4,true],\"post_proc\":[\"/\",\".cal\"]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2,true,false],\"post_proc\":[\">\",4,\"-\",2,\"*\",10]}}}";
 /*R""""(
 {
    "brand":"iNode",
    "model":"Energy Meter",
    "model_id":"INEM",
-   "condition":["manufacturerdata", "index", 0, "9082"],
+   "condition":["manufacturerdata", 26, "index", 0, "9082"],
    "properties":{
       ".cal":{
          "decoder":["value_from_hex_data", "manufacturerdata", 16, 4, true],
