bea5c6bce77ac9502569457716eb99b583e45a5f
h2zero <powellperalta@gmail.com>
2021-12-08 21:32:13 -0700

    Extend condition checks for service/manufacturer data length.
    This adds an extra operator parameter when checking the length of the service/manufacturer data.
    
    Valid inputs are ">" , ">=" , "=" , "<" , "<=".
    
    Usage: `"condition":["manufacturerdata", "=", 50, "index", 0, "4c00"],`

--- src/decoder.h
+++ src/decoder.h
@@ -43,6 +43,7 @@ private:
   void reverse_hex_data(const char* in, char* out, int l);
   long value_from_hex_string(const char* data_str, int offset, int data_length, bool reverse, bool canBeNegative = true);
   bool data_index_is_valid(const char* str, size_t index, size_t len);
+  int data_length_is_valid(size_t data_len, size_t default_min, JsonArray& condition, int idx);
   std::string sanitizeJsonKey(const char* key_in);
 
   size_t m_docMax = 4096;
