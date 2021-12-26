e17f8089b61092e9f41f1cd790194a616a38539e
DigiH <github@digih.net>
2021-12-26 20:02:05 +0100

    Update common_props.h

--- src/devices/common_props.h
+++ src/devices/common_props.h
@@ -59,17 +59,13 @@ const char* _common_BVTH_props = "{\"properties\":{\"batt\":{\"unit\":\"%\",\"na
    }
 })"""";*/
 
-const char* _common_weight_props = "{\"properties\":{\"weight\":{\"unit\":\"kg\",\"name\":\"weight\"},\"impedance\":{\"unit\":\"Ohm\",\"name\":\"impedance\"}}}";
+const char* _common_weight_props = "{\"properties\":{\"weight\":{\"unit\":\"kg\",\"name\":\"weight\"}}}";
 /*R""""(
 {
    "properties":{
       "weight":{
          "unit":"kg",
          "name":"weight"
-      },
-      "impedance":{
-         "unit":"Ohm",
-         "name":"impedance"
       }
    }
 })"""";*/
