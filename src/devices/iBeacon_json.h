092eea09ae73c910694126f33470da6986dae3db
DigiH <github@digih.net>
2021-12-29 16:51:58 +0100

    Update iBeacon_json.h

--- src/devices/iBeacon_json.h
+++ src/devices/iBeacon_json.h
@@ -25,6 +25,30 @@ const char* _ibeacon_json = R""""(
          "condition":["manufacturerdata", 48, "9"],
          "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
       },
+      "__power":{
+         "condition":["manufacturerdata", 48, "a"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
+      },
+      "___power":{
+         "condition":["manufacturerdata", 48, "b"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
+      },
+      "____power":{
+         "condition":["manufacturerdata", 48, "c"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
+      },
+      "_____power":{
+         "condition":["manufacturerdata", 48, "d"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
+      },
+      "______power":{
+         "condition":["manufacturerdata", 48, "e"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
+      },
+      "_______power":{
+         "condition":["manufacturerdata", 48, "f"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
+      },
       "battery":{
          "condition":["manufacturerdata", 48, "0"],
          "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
@@ -37,6 +61,31 @@ const char* _ibeacon_json = R""""(
       },
       "__battery":{
          "condition":["manufacturerdata", 48, "2"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
+         "post_proc":["/", 10]
+      },
+      "___battery":{
+         "condition":["manufacturerdata", 48, "3"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
+         "post_proc":["/", 10]
+      },
+      "____battery":{
+         "condition":["manufacturerdata", 48, "4"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
+         "post_proc":["/", 10]
+      },
+      "_____battery":{
+         "condition":["manufacturerdata", 48, "5"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
+         "post_proc":["/", 10]
+      },
+      "______battery":{
+         "condition":["manufacturerdata", 48, "6"],
+         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
+         "post_proc":["/", 10]
+      },
+      "______battery":{
+         "condition":["manufacturerdata", 48, "7"],
          "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
          "post_proc":["/", 10]
       }
