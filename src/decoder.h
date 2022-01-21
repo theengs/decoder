3171cc6c25870ab173ef47253caf4db1cadcb341
h2zero <32826625+h2zero@users.noreply.github.com>
2022-01-21 04:48:12 -0700

    Add bf_value_from_hex_string function. (#56)
    * Add bf_value_from_hex_string function.
    
    Adds a new function to decode values that are in the binary fraction format of 0xXX.XX fractional values.
    
    This is to be used as a separate decoder such as:
    ```
    "tempc":{
       "decoder":["bf_value_from_hex_data", "manufacturerdata", 8, 4, false, true]
    }
    ```
    
    # Conflicts:
    #	src/decoder.cpp
    
    * Update docs

--- src/decoder.h
+++ src/decoder.h
@@ -44,7 +44,8 @@ public:
 
 private:
   void reverse_hex_data(const char* in, char* out, int l);
-  long value_from_hex_string(const char* data_str, int offset, int data_length, bool reverse, bool canBeNegative = true);
+  double value_from_hex_string(const char* data_str, int offset, int data_length, bool reverse, bool canBeNegative = true);
+  double bf_value_from_hex_string(const char* data_str, int offset, int data_length, bool reverse, bool canBeNegative = true);
   bool data_index_is_valid(const char* str, size_t index, size_t len);
   int data_length_is_valid(size_t data_len, size_t default_min, JsonArray& condition, int idx);
   std::string sanitizeJsonKey(const char* key_in);
