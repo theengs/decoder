564044fb6d6e1cbe99f4e3857cbd7b78f198da27
DigiH <github@digih.net>
2022-02-22 01:22:13 +0100

    Update iBeacon_json.h
    Optimised iBeacon decoder
    
    • Condensed the power and volt properties with multiple conditions
    • renamed battery property to volt to bring in line with other voltage properties
    
    Update iBeacon_json.h

--- src/devices/iBeacon_json.h
+++ src/devices/iBeacon_json.h
@@ -1,4 +1,4 @@
-const char* _ibeacon_json = "{\"brand\":\"GENERIC\",\"model\":\"IBEACON\",\"model_id\":\"IBEACON\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c00\"],\"properties\":{\"mfid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",0,4]},\"uuid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",8,32]},\"major\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",40,4,false]},\"minor\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",44,4,false]},\"power\":{\"condition\":[\"manufacturerdata\",48,\"8\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"_power\":{\"condition\":[\"manufacturerdata\",48,\"9\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"__power\":{\"condition\":[\"manufacturerdata\",48,\"a\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"___power\":{\"condition\":[\"manufacturerdata\",48,\"b\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"____power\":{\"condition\":[\"manufacturerdata\",48,\"c\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"_____power\":{\"condition\":[\"manufacturerdata\",48,\"d\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"______power\":{\"condition\":[\"manufacturerdata\",48,\"e\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"_______power\":{\"condition\":[\"manufacturerdata\",48,\"f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"battery\":{\"condition\":[\"manufacturerdata\",48,\"0\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]},\"_battery\":{\"condition\":[\"manufacturerdata\",48,\"1\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]},\"__battery\":{\"condition\":[\"manufacturerdata\",48,\"2\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]},\"___battery\":{\"condition\":[\"manufacturerdata\",48,\"3\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]},\"____battery\":{\"condition\":[\"manufacturerdata\",48,\"4\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]},\"_____battery\":{\"condition\":[\"manufacturerdata\",48,\"5\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]}}}";
+const char* _ibeacon_json = "{\"brand\":\"GENERIC\",\"model\":\"IBEACON\",\"model_id\":\"IBEACON\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c00\"],\"properties\":{\"mfid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",0,4]},\"uuid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",8,32]},\"major\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",40,4,false]},\"minor\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",44,4,false]},\"power\":{\"condition\":[\"manufacturerdata\",48,\"8\",\"|\",\"manufacturerdata\",48,\"9\",\"|\",\"manufacturerdata\",48,\"a\",\"|\",\"manufacturerdata\",48,\"b\",\"|\",\"manufacturerdata\",48,\"c\",\"|\",\"manufacturerdata\",48,\"d\",\"|\",\"manufacturerdata\",48,\"e\",\"|\",\"manufacturerdata\",48,\"f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"volt\":{\"condition\":[\"manufacturerdata\",48,\"0\",\"|\",\"manufacturerdata\",48,\"1\",\"|\",\"manufacturerdata\",48,\"2\",\"|\",\"manufacturerdata\",48,\"3\",\"|\",\"manufacturerdata\",48,\"4\",\"|\",\"manufacturerdata\",48,\"5\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]}}}";
 
 /*R""""(
 {
@@ -20,71 +20,18 @@ const char* _ibeacon_json = "{\"brand\":\"GENERIC\",\"model\":\"IBEACON\",\"mode
          "decoder":["value_from_hex_data", "manufacturerdata", 44, 4, false]
       },
       "power":{
-         "condition":["manufacturerdata", 48, "8"],
+         "condition":["manufacturerdata", 48, "8", "|", "manufacturerdata", 48, "9", "|", "manufacturerdata", 48, "a", "|", "manufacturerdata", 48, "b", "|", "manufacturerdata", 48, "c", "|", "manufacturerdata", 48, "d", "|", "manufacturerdata", 48, "e", "|", "manufacturerdata", 48, "f"],
          "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
       },
-      "_power":{
-         "condition":["manufacturerdata", 48, "9"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
-      },
-      "__power":{
-         "condition":["manufacturerdata", 48, "a"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
-      },
-      "___power":{
-         "condition":["manufacturerdata", 48, "b"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
-      },
-      "____power":{
-         "condition":["manufacturerdata", 48, "c"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
-      },
-      "_____power":{
-         "condition":["manufacturerdata", 48, "d"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
-      },
-      "______power":{
-         "condition":["manufacturerdata", 48, "e"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
-      },
-      "_______power":{
-         "condition":["manufacturerdata", 48, "f"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
-      },
-      "battery":{
-         "condition":["manufacturerdata", 48, "0"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
-         "post_proc":["/", 10]
-      },
-      "_battery":{
-         "condition":["manufacturerdata", 48, "1"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
-         "post_proc":["/", 10]
-      },
-      "__battery":{
-         "condition":["manufacturerdata", 48, "2"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
-         "post_proc":["/", 10]
-      },
-      "___battery":{
-         "condition":["manufacturerdata", 48, "3"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
-         "post_proc":["/", 10]
-      },
-      "____battery":{
-         "condition":["manufacturerdata", 48, "4"],
-         "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
-         "post_proc":["/", 10]
-      },
-      "_____battery":{
-         "condition":["manufacturerdata", 48, "5"],
+      "volt":{
+         "condition":["manufacturerdata", 48, "0", "|", "manufacturerdata", 48, "1", "|", "manufacturerdata", 48, "2", "|", "manufacturerdata", 48, "3", "|", "manufacturerdata", 48, "4", "|", "manufacturerdata", 48, "5"],
          "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
          "post_proc":["/", 10]
       }
    }
 })"""";*/
 
-const char* _ibeacon_json_props = "{\"properties\":{\"mfid\":{\"unit\":\"hex\",\"name\":\"manufacturer id\"},\"uuid\":{\"unit\":\"hex\",\"name\":\"service uuid\"},\"major\":{\"unit\":\"hex\",\"name\":\"major value\"},\"minor\":{\"unit\":\"hex\",\"name\":\"minor value\"},\"power\":{\"unit\":\"int\",\"name\":\"tx power\"},\"battery\":{\"unit\":\"Volt\",\"name\":\"battery\"}}}";
+const char* _ibeacon_json_props = "{\"properties\":{\"mfid\":{\"unit\":\"hex\",\"name\":\"manufacturer id\"},\"uuid\":{\"unit\":\"hex\",\"name\":\"service uuid\"},\"major\":{\"unit\":\"hex\",\"name\":\"major value\"},\"minor\":{\"unit\":\"hex\",\"name\":\"minor value\"},\"power\":{\"unit\":\"int\",\"name\":\"tx power\"},\"volt\":{\"unit\":\"V\",\"name\":\"voltage\"}}}";
 /*R""""(
 {
    "properties":{
@@ -107,10 +54,10 @@ const char* _ibeacon_json_props = "{\"properties\":{\"mfid\":{\"unit\":\"hex\",\
       "power":{
          "unit":"int",
          "name":"tx power"
-      }.
-      "battery":{
-         "unit":"Volt",
-         "name":"battery"
+      },
+      "volt":{
+         "unit":"V",
+         "name":"voltage"
       }
    }
 })"""";*/
