1c7c24d77198125a742d619e1fe1454d2190c361
Florian <1technophile@users.noreply.github.com>
2022-03-06 10:39:36 -0600

    Add new version of IBT-2X with 24 manufacturerdata length

--- src/devices/IBT_2X_json.h
+++ src/devices/IBT_2X_json.h
@@ -1,18 +1,27 @@
-const char* _IBT_2X_json = "{\"brand\":\"Inkbird\",\"model\":\"iBBQ\",\"model_id\":\"IBT-2X\",\"condition\":[\"name\",\"index\",0,\"iBBQ\",\"&\",\"manufacturerdata\",\"=\",24,\"index\",0,\"0000\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",16,4,true,false],\"post_proc\":[\"/\",10]},\"tempc2\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,4,true,false],\"post_proc\":[\"/\",10]}}}";
-
+const char* _IBT_2X_json = "{\"brand\":\"Inkbird\",\"model\":\"iBBQ\",\"model_id\":\"IBT-2X\",\"condition\":[\"name\",\"index\",0,\"iBBQ\",\"&\",\"manufacturerdata\",\">=\",24,\"index\",0,\"0000\",\"&\",\"manufacturerdata\",\"<=\",28,\"index\",0,\"0000\"],\"properties\":{\"tempc\":{\"condition\":[\"manufacturerdata\",0,\"00000000\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",16,4,true,false],\"post_proc\":[\"/\",10]},\"_tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,4,true,false],\"post_proc\":[\"/\",10]},\"tempc2\":{\"condition\":[\"manufacturerdata\",0,\"00000000\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,4,true,false],\"post_proc\":[\"/\",10]},\"_tempc2\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,4,true,false],\"post_proc\":[\"/\",10]}}}";
 /*R""""(
 {
    "brand":"Inkbird",
    "model":"iBBQ",
    "model_id":"IBT-2X",
-   "condition":["name", "index", 0, "iBBQ","&","manufacturerdata", "=", 24, "index", 0, "0000"],
+   "condition":["name", "index", 0, "iBBQ","&","manufacturerdata", ">=", 24, "index", 0, "0000","&","manufacturerdata", "<=", 28, "index", 0, "0000"],
    "properties":{
       "tempc":{
+         "condition":["manufacturerdata", 0, "00000000"],
          "decoder":["value_from_hex_data", "manufacturerdata", 16, 4, true, false],
          "post_proc":["/", 10]
       },
-      "tempc2":{
+      "_tempc":{
          "decoder":["value_from_hex_data", "manufacturerdata", 20, 4, true, false],
+         "post_proc":["/", 10]
+      },
+      "tempc2":{
+         "condition":["manufacturerdata", 0, "00000000"],
+         "decoder":["value_from_hex_data", "manufacturerdata", 20, 4, true, false],
+         "post_proc":["/", 10]
+      },
+      "_tempc2":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 24, 4, true, false],
          "post_proc":["/", 10]
       }
    }
