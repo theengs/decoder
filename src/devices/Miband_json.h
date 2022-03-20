e66adc746556240f6c6d5ff04cce50c4cac5e603
DigiH <github@digih.net>
2022-03-20 18:26:19 +0100

    Initial NOT implementation (#97)

--- src/devices/Miband_json.h
+++ src/devices/Miband_json.h
@@ -1,4 +1,4 @@
-const char* _Miband_json = "{\"brand\":\"Xiaomi\",\"model\":\"Miband\",\"model_id\":\"MiBand\",\"condition\":[\"uuid\",\"contain\",\"fee0\"],\"properties\":{\"steps\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",0,4,true,false]},\"act_bpm\":{\"condition\":[\"manufacturerdata\",10,\"3\",\"|\",\"manufacturerdata\",10,\"4\",\"|\",\"manufacturerdata\",10,\"5\",\"|\",\"manufacturerdata\",10,\"6\",\"|\",\"manufacturerdata\",10,\"7\",\"|\",\"manufacturerdata\",10,\"8\",\"|\",\"manufacturerdata\",10,\"9\",\"|\",\"manufacturerdata\",10,\"a\",\"|\",\"manufacturerdata\",10,\"b\",\"|\",\"manufacturerdata\",10,\"c\",\"|\",\"manufacturerdata\",10,\"d\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,2,false,false]}}}";
+const char* _Miband_json = "{\"brand\":\"Xiaomi\",\"model\":\"Miband\",\"model_id\":\"MiBand\",\"condition\":[\"uuid\",\"contain\",\"fee0\"],\"properties\":{\"steps\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",0,4,true,false]},\"act_bpm\":{\"condition\":[\"manufacturerdata\",10,\"!\",\"f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,2,false,false]}}}";
 /*R""""(
 {
    "brand":"Xiaomi",
@@ -10,7 +10,7 @@ const char* _Miband_json = "{\"brand\":\"Xiaomi\",\"model\":\"Miband\",\"model_i
          "decoder":["value_from_hex_data", "servicedata", 0, 4, true, false]
       },
       "act_bpm":{
-         "condition":["manufacturerdata", 10, "3", "|", "manufacturerdata", 10, "4", "|", "manufacturerdata", 10, "5", "|", "manufacturerdata", 10, "6", "|", "manufacturerdata", 10, "7", "|", "manufacturerdata", 10, "8", "|", "manufacturerdata", 10, "9", "|", "manufacturerdata", 10, "a", "|", "manufacturerdata", 10, "b", "|", "manufacturerdata", 10, "c", "|", "manufacturerdata", 10, "d"],
+         "condition":["manufacturerdata", 10, "!", "f"],
          "decoder":["value_from_hex_data", "manufacturerdata", 10, 2, false, false]
       }
    }
