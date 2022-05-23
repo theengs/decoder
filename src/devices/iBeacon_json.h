54e711a26f11c71af2cd77e1b9c33dc445361146
DigiH <github@digih.net>
2022-05-23 15:30:29 +0200

    Binary Bit optimisations
    Reducing some decoders' footprint by replacing stringed OR conditions with new direct bit evaluations.

--- src/devices/iBeacon_json.h
+++ src/devices/iBeacon_json.h
@@ -1,9 +1,9 @@
-const char* _ibeacon_json = "{\"brand\":\"GENERIC\",\"model\":\"IBEACON\",\"model_id\":\"IBEACON\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c00\"],\"properties\":{\"mfid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",0,4]},\"uuid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",8,32]},\"major\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",40,4,false]},\"minor\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",44,4,false]},\"power\":{\"condition\":[\"manufacturerdata\",48,\"8\",\"|\",\"manufacturerdata\",48,\"9\",\"|\",\"manufacturerdata\",48,\"a\",\"|\",\"manufacturerdata\",48,\"b\",\"|\",\"manufacturerdata\",48,\"c\",\"|\",\"manufacturerdata\",48,\"d\",\"|\",\"manufacturerdata\",48,\"e\",\"|\",\"manufacturerdata\",48,\"f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"volt\":{\"condition\":[\"manufacturerdata\",48,\"0\",\"|\",\"manufacturerdata\",48,\"1\",\"|\",\"manufacturerdata\",48,\"2\",\"|\",\"manufacturerdata\",48,\"3\",\"|\",\"manufacturerdata\",48,\"4\",\"|\",\"manufacturerdata\",48,\"5\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]}}}";
+const char* _ibeacon_json = "{\"brand\":\"GENERIC\",\"model\":\"iBeacon\",\"model_id\":\"IBEACON\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c00\"],\"properties\":{\"mfid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",0,4]},\"uuid\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",8,32]},\"major\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",40,4,false]},\"minor\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",44,4,false]},\"power\":{\"condition\":[\"manufacturerdata\",48,\"bit\",3,1],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"volt\":{\"condition\":[\"manufacturerdata\",48,\"bit\",3,0],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false],\"post_proc\":[\"/\",10]}}}";
 
 /*R""""(
 {
    "brand":"GENERIC",
-   "model":"IBEACON",
+   "model":"iBeacon",
    "model_id":"IBEACON",
    "condition":["manufacturerdata", "=", 50, "index", 0, "4c00"],
    "properties":{
@@ -20,11 +20,11 @@ const char* _ibeacon_json = "{\"brand\":\"GENERIC\",\"model\":\"IBEACON\",\"mode
          "decoder":["value_from_hex_data", "manufacturerdata", 44, 4, false]
       },
       "power":{
-         "condition":["manufacturerdata", 48, "8", "|", "manufacturerdata", 48, "9", "|", "manufacturerdata", 48, "a", "|", "manufacturerdata", 48, "b", "|", "manufacturerdata", 48, "c", "|", "manufacturerdata", 48, "d", "|", "manufacturerdata", 48, "e", "|", "manufacturerdata", 48, "f"],
+         "condition":["manufacturerdata", 48, "bit", 3, 1],
          "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false]
       },
       "volt":{
-         "condition":["manufacturerdata", 48, "0", "|", "manufacturerdata", 48, "1", "|", "manufacturerdata", 48, "2", "|", "manufacturerdata", 48, "3", "|", "manufacturerdata", 48, "4", "|", "manufacturerdata", 48, "5"],
+         "condition":["manufacturerdata", 48, "bit", 3, 0],
          "decoder":["value_from_hex_data","manufacturerdata", 48, 2, false],
          "post_proc":["/", 10]
       }
