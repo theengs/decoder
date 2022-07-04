bb676552dc563c07dd16c5722e8463c7553e9ee7
DigiH <github@digih.net>
2022-07-04 16:29:08 +0200

    TH2-P01B extension

--- src/devices/IBS_TH2_json.h
+++ src/devices/IBS_TH2_json.h
@@ -1,14 +1,19 @@
-const char* _IBS_TH2_json = "{\"brand\":\"Inkbird\",\"model\":\"T Sensor\",\"model_id\":\"IBS-TH2\",\"condition\":[\"name\",\"index\",0,\"tps\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",0,4,true],\"post_proc\":[\"/\",100]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",14,2,false,false]}}}";
+const char* _IBS_TH2_json = "{\"brand\":\"Inkbird\",\"model\":\"TH Sensor\",\"model_id\":\"IBS-TH2/P01B\",\"condition\":[\"name\",\"index\",0,\"tps\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",0,4,true],\"post_proc\":[\"/\",100]},\"hum\":{\"condition\":[\"manufacturerdata\",4,\"!\",\"ffff\",\"&\",\"manufacturerdata\",4,\"!\",\"0000\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,4,true,false],\"post_proc\":[\"/\",100]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",14,2,false,false]}}}";
 
 /*R""""(
 {
    "brand":"Inkbird",
    "model":"TH Sensor",
-   "model_id":"IBS-TH2",
+   "model_id":"IBS-TH2/P01B",
    "condition":["name", "index", 0, "tps"],
    "properties":{
       "tempc":{
          "decoder":["value_from_hex_data", "manufacturerdata", 0, 4, true],
+         "post_proc":["/", 100]
+      },
+      "hum":{
+         "condition":["manufacturerdata", 4, "!", "ffff", "&", "manufacturerdata", 4, "!", "0000"],
+         "decoder":["value_from_hex_data", "manufacturerdata", 4, 4, true, false],
          "post_proc":["/", 100]
       },
       "batt":{
@@ -17,17 +22,4 @@ const char* _IBS_TH2_json = "{\"brand\":\"Inkbird\",\"model\":\"T Sensor\",\"mod
    }
 })"""";*/
 
-const char* _IBS_TH2_json_props = "{\"properties\":{\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"}}}";
-/*R""""(
-{
-   "properties":{
-      "batt":{
-         "unit":"%",
-         "name":"battery"
-      },
-      "tempc":{
-         "unit":"°C",
-         "name":"temperature"
-      }
-   }
-})"""";*/
+const char* _IBS_TH2_json_props = _common_BTH_props;
