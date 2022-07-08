08dcc5e792ce2968095ff2b988699d9c6ed6ac50
Koen Vervloesem <koen@vervloesem.eu>
2022-07-08 09:14:16 +0200

    Detect battery percentage in LYWSD02 decoder

--- src/devices/LYWSD02_json.h
+++ src/devices/LYWSD02_json.h
@@ -1,6 +1,6 @@
 #include "common_props.h"
 
-const char* _LYWSD02_json = "{\"brand\":\"Xiaomi\",\"model\":\"Cleargrass clock\",\"model_id\":\"LYWSD02\",\"condition\":[\"servicedata\",\"index\",2,\"205b04\"],\"properties\":{\"tempc\":{\"condition\":[\"servicedata\",25,\"4\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"condition\":[\"servicedata\",25,\"6\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true,false],\"post_proc\":[\"/\",10]}}}";
+const char* _LYWSD02_json = "{\"brand\":\"Xiaomi\",\"model\":\"Cleargrass clock\",\"model_id\":\"LYWSD02\",\"condition\":[\"servicedata\",\"index\",2,\"205b04\"],\"properties\":{\"tempc\":{\"condition\":[\"servicedata\",24,\"0410\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"condition\":[\"servicedata\",24,\"0610\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true,false],\"post_proc\":[\"/\",10]},\"batt\":{\"condition\":[\"servicedata\",24,\"0a10\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,2,false,false]}}}";
 /* R""""(
 {
    "brand":"Xiaomi",
@@ -9,14 +9,18 @@ const char* _LYWSD02_json = "{\"brand\":\"Xiaomi\",\"model\":\"Cleargrass clock\
    "condition":["servicedata", "index", 2, "205b04"],
    "properties":{
       "tempc":{
-         "condition":["servicedata", 25, "4"],
+         "condition":["servicedata", 24, "0410"],
          "decoder":["value_from_hex_data", "servicedata", 30, 4, true],
          "post_proc":["/", 10]
       },
       "hum":{
-         "condition":["servicedata", 25, "6"],
+         "condition":["servicedata", 24, "0610"],
          "decoder":["value_from_hex_data", "servicedata", 30, 4, true, false],
          "post_proc":["/", 10]
+      },
+      "batt":{
+         "condition":["servicedata", 24, "0a10"],
+         "decoder":["value_from_hex_data", "servicedata", 30, 2, false, false]
       }
    }
 })"""";*/
