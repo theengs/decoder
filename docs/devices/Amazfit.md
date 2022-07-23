40e9b7d819dc4021afb92018114b1da605ff0fe6
DigiH <github@digih.net>
2022-07-23 23:00:40 +0200

    Amazfit inclusion in Mi Band (#161)

---
+++ docs/devices/Amazfit.md
@@ -0,0 +1,12 @@
+# Amazfit Smart Watch
+
+|Model Id|[Amazfit](https://github.com/theengs/decoder/blob/development/src/devices/Miband_json.h)|
+|-|-|
+|Brand|Amazfit|
+|Model|Smart Watch|
+|Short Description|Various Amazfit Smart Watch models with step count and activity heart rate monitoring|
+|Communication|BLE broadcast|
+|Frequency|2.4Ghz|
+|Power source|Rechargeable battery|
+|Exchanged data|steps, activity heart rate (when activated in the Zepp Life settings)|
+|Encrypted|No|
--- src/devices/Miband_json.h
+++ src/devices/Miband_json.h
@@ -1,9 +1,9 @@
-const char* _Miband_json = "{\"brand\":\"Xiaomi\",\"model\":\"Miband\",\"model_id\":\"MiBand\",\"condition\":[\"uuid\",\"contain\",\"fee0\"],\"properties\":{\"steps\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",0,4,true,false]},\"act_bpm\":{\"condition\":[\"manufacturerdata\",10,\"!\",\"f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,2,false,false]}}}";
+const char* _Miband_json = "{\"brand\":\"Xiaomi/Amazfit\",\"model\":\"Mi Band/Smart Watch\",\"model_id\":\"MB/SW\",\"condition\":[\"uuid\",\"contain\",\"fee0\"],\"properties\":{\"steps\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",0,4,true,false]},\"act_bpm\":{\"condition\":[\"manufacturerdata\",10,\"!\",\"f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,2,false,false]}}}";
 /*R""""(
 {
-   "brand":"Xiaomi",
-   "model":"Miband",
-   "model_id":"MiBand",
+   "brand":"Xiaomi/Amazfit",
+   "model":"Mi Band/Smart Watch",
+   "model_id":"MB/SW",
    "condition":["uuid", "contain", "fee0"],
    "properties":{
       "steps":{
