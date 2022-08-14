e8e9a258eaac211962939597c3f61dc33ae6bac7
DigiH <github@digih.net>
2022-08-14 21:25:21 +0200

    CGD1 clarification (#180)
    Docs updates

--- docs/devices/CGD1.md
+++ docs/devices/CGD1.md
@@ -1,12 +1,12 @@
-# Cleargrass CGD1
+# Cleargrass CGD1 Alarm Clock
 
 |Model Id|[CGD1](https://github.com/theengs/decoder/blob/development/src/devices/CGD1_json.h)|
 |-|-|
-|Brand|CLEARGRASS|
-|Model|Alarm clock|
+|Brand|Cleargrass|
+|Model|Alarm Clock|
 |Short Description| Blue, green or white alarm clock with temperature and humidity sensors. Trigger a blue backlight when pressed|
 |Communication|BLE broadcast|
 |Frequency|2.4Ghz|
-|Power source|Battery 3 AA|
-|Exchanged data|battery, humidity, temperature|
+|Power source|Battery 2 AA|
+|Exchanged data|temperature, humidity|
 |Encrypted|No|
--- src/devices/CGD1_json.h
+++ src/devices/CGD1_json.h
@@ -1,11 +1,11 @@
 #include "common_props.h"
 
-const char* _CGD1_json = "{\"brand\":\"Xiaomi\",\"model\":\"CG_alarm_clock\",\"model_id\":\"CGD1\",\"condition\":[\"servicedata\",\"index\",0,\"080caf\",\"|\",\"servicedata\",\"index\",0,\"080c09\",\"|\",\"servicedata\",\"index\",0,\"080cd0\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,4,true,false],\"post_proc\":[\"/\",10]}}}";
+const char* _CGD1_json = "{\"brand\":\"Cleargrass\",\"model\":\"Alarm Clock\",\"model_id\":\"CGD1\",\"condition\":[\"servicedata\",\"index\",0,\"080caf\",\"|\",\"servicedata\",\"index\",0,\"080c09\",\"|\",\"servicedata\",\"index\",0,\"080cd0\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,4,true,false],\"post_proc\":[\"/\",10]}}}";
 
 /*R""""(
 {
-   "brand":"Xiaomi",
-   "model":"CG_alarm_clock",
+   "brand":"Cleargrass",
+   "model":"Alarm Clock",
    "model_id":"CGD1",
    "condition":["servicedata", "index", 0, "080caf", "|", "servicedata", "index", 0, "080c09", "|", "servicedata", "index", 0, "080cd0"],
    "properties":{
