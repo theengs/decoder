f784e0ca8011d0db2d34c45b8a8dc5277c135488
DigiH <github@digih.net>
2022-08-25 11:56:31 +0200

    Update version to 0.7.0 (#186)

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="TheengsDecoder",
-    version="0.6.2",
+    version="0.7.0",
     description="A message decoder for the Internet of Things",
     author='Theengs',
     license=" GPL-3.0 License",
