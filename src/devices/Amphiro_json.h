67d889fe2b60a6129856f532bdea7bfcc25936ac
DigiH <github@digih.net>
2022-09-06 15:49:10 +0200

    Amphiro adjustments (#197)

--- src/devices/Amphiro_json.h
+++ src/devices/Amphiro_json.h
@@ -1,4 +1,4 @@
-const char* _AMPHIRO_json = "{\"brand\":\"Oras\",\"model\":\"Hydractiva Digital\",\"model_id\":\"ADHS\",\"cidc\":false,\"condition\":[\"manufacturerdata\",\"=\",42,\"index\",0,\"eefa\"],\"properties\":{\"sessions\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,6,false,false]},\"time\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,4,false,false]},\"litres\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,6,false,false],\"post_proc\":[\"/\",2560]},\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",26,2,false,false]},\"energy\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",28,4,false,false],\"post_proc\":[\"/\",100]}}}";
+const char* _AMPHIRO_json = "{\"brand\":\"Oras\",\"model\":\"Hydractiva Digital\",\"model_id\":\"ADHS\",\"cidc\":false,\"condition\":[\"manufacturerdata\",\"=\",42,\"index\",0,\"eefa\"],\"properties\":{\"session\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,6,false,false]},\"seconds\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,4,false,false]},\"litres\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,6,false,false],\"post_proc\":[\"/\",2560]},\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",26,2,false,false]},\"energy\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",28,4,false,false],\"post_proc\":[\"/\",100]}}}";
 /*R""""(
 {
    "brand":"Oras",
@@ -7,10 +7,10 @@ const char* _AMPHIRO_json = "{\"brand\":\"Oras\",\"model\":\"Hydractiva Digital\
    "cidc":false,
    "condition":["manufacturerdata", "=", 42, "index", 0, "eefa"],
    "properties":{
-      "sessions":{
+      "session":{
          "decoder":["value_from_hex_data", "manufacturerdata", 4, 6, false, false]
       },
-      "time":{
+      "seconds":{
          "decoder":["value_from_hex_data", "manufacturerdata", 10, 4, false, false]
       },
       "litres":{
@@ -27,21 +27,21 @@ const char* _AMPHIRO_json = "{\"brand\":\"Oras\",\"model\":\"Hydractiva Digital\
    }
 })"""";*/
 
-const char* _AMPHIRO_json_props = "{\"properties\":{\"sessions\":{\"unit\":\"int\",\"name\":\"sessions\"},\"time\":{\"unit\":\"s\",\"name\":\"time\"},\"litres\":{\"unit\":\"L\",\"name\":\"litres\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"energy\":{\"unit\":\"kWh\",\"name\":\"energy\"}}}";
+const char* _AMPHIRO_json_props = "{\"properties\":{\"session\":{\"unit\":\"int\",\"name\":\"session\"},\"seconds\":{\"unit\":\"s\",\"name\":\"duration\"},\"litres\":{\"unit\":\"L\",\"name\":\"volume\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"energy\":{\"unit\":\"kWh\",\"name\":\"energy\"}}}";
 /*R""""(
 {
    "properties":{
-      "sessions":{
+      "session":{
          "unit":"int",
-         "name":"sessions"
+         "name":"session"
       },
-      "time":{
+      "seconds":{
          "unit":"s",
-         "name":"time"
+         "name":"duration"
       },
       "litres":{
          "unit":"L",
-         "name":"litres"
+         "name":"volume"
       },
       "tempc": {
          "unit": "°C",
