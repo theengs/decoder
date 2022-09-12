6a649368ec0f48bda198d8983da9a70d9e9062bd
DigiH <github@digih.net>
2022-09-12 20:59:49 +0200

    CGPR1 enhancments (#200)

--- src/devices/CGPR1_json.h
+++ src/devices/CGPR1_json.h
@@ -1,24 +1,36 @@
-const char* _CGPR1_json = "{\"brand\":\"Qingping\",\"model\":\"Motion & Light\",\"model_id\":\"CGPR1\",\"condition\":[\"servicedata\",\"=\",40,\"index\",2,\"12\",\"|\",\"servicedata\",\"=\",34,\"index\",2,\"12\",\"&\",\"uuid\",\"index\",0,\"fdcd\"],\"properties\":{\"lux\":{\"condition\":[\"servicedata\",\"=\",40],\"decoder\":[\"value_from_hex_data\",\"servicedata\",33,4,true]},\"pres\":{\"condition\":[\"servicedata\",\"=\",34],\"decoder\":[\"bit_static_value\",\"servicedata\",21,1,true,false]}}}";
+const char* _CGPR1_json = "{\"brand\":\"Qingping\",\"model\":\"Motion & Light\",\"model_id\":\"CGPR1\",\"condition\":[\"servicedata\",\"=\",28,\"index\",2,\"12\",\"|\",\"servicedata\",\"=\",34,\"index\",2,\"12\",\"|\",\"servicedata\",\"=\",40,\"index\",2,\"12\",\"&\",\"uuid\",\"index\",0,\"fdcd\"],\"properties\":{\"lux\":{\"condition\":[\"servicedata\",\"=\",40],\"decoder\":[\"value_from_hex_data\",\"servicedata\",32,4,true,false]},\"_lux\":{\"condition\":[\"servicedata\",\"=\",34],\"decoder\":[\"value_from_hex_data\",\"servicedata\",22,4,true,false]},\"pres\":{\"condition\":[\"servicedata\",\"=\",34],\"decoder\":[\"bit_static_value\",\"servicedata\",21,0,false,true]},\"_pres\":{\"condition\":[\"servicedata\",\"=\",28],\"decoder\":[\"bit_static_value\",\"servicedata\",21,0,false,true]},\"batt\":{\"condition\":[\"servicedata\",\"=\",40],\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,2,false,false]}}}";
 /*
 R""""(
 {
    "brand":"Qingping",
    "model":"Motion & Light",
    "model_id":"CGPR1",
-   "condition":["servicedata", "=", 40, "index", 2, "12", "|", "servicedata", "=", 34, "index", 2, "12", "&", "uuid", "index", 0, "fdcd"],
+   "condition":["servicedata", "=", 28, "index", 2, "12", "|", "servicedata", "=", 34, "index", 2, "12", "|", "servicedata", "=", 40, "index", 2, "12", "&", "uuid", "index", 0, "fdcd"],
    "properties":{
       "lux":{
          "condition":["servicedata", "=", 40],
-         "decoder":["value_from_hex_data", "servicedata", 33, 4, true]
+         "decoder":["value_from_hex_data", "servicedata", 32, 4, true, false]
+      },
+      "_lux":{
+         "condition":["servicedata", "=", 34],
+         "decoder":["value_from_hex_data", "servicedata", 22, 4, true, false]
       },
       "pres":{
          "condition":["servicedata", "=", 34],
-         "decoder":["bit_static_value", "servicedata", 21, 1, true, false]
+         "decoder":["bit_static_value", "servicedata", 21, 0, false, true]
+      },
+      "_pres":{
+         "condition":["servicedata", "=", 28],
+         "decoder":["bit_static_value", "servicedata", 21, 0, false, true]
+      },
+      "batt":{
+         "condition":["servicedata", "=", 40],
+         "decoder":["value_from_hex_data", "servicedata", 20, 2, false, false]
       }
    }
 })"""";*/
 
-const char* _CGPR1_json_props = "{\"properties\":{\"lux\":{\"unit\":\"lx\",\"name\":\"illuminance\"},\"pres\":{\"unit\":\"status\",\"name\":\"presence\"}}}";
+const char* _CGPR1_json_props = "{\"properties\":{\"lux\":{\"unit\":\"lx\",\"name\":\"illuminance\"},\"pres\":{\"unit\":\"status\",\"name\":\"presence\"},\"batt\":{\"unit\":\"%\",\"name\":\"battery\"}}}";
 /*R""""(
 {
    "properties":{
@@ -29,6 +41,10 @@ const char* _CGPR1_json_props = "{\"properties\":{\"lux\":{\"unit\":\"lx\",\"nam
       "pres":{
          "unit":"status",
          "name":"presence"
+      },
+      "batt":{
+         "unit":"%",
+         "name":"battery"
       }
    }
 })"""";*/
