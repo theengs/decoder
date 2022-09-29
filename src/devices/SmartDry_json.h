d25c1929b075aa217da9e2bc601a2456153e2211
DigiH <github@digih.net>
2022-09-29 16:54:16 +0200

    SmartDry shake changes
    • deleted boolean shake property
    • adjusted and renamed shake_count property to current shake integer

--- src/devices/SmartDry_json.h
+++ src/devices/SmartDry_json.h
@@ -1,5 +1,4 @@
-const char* _SmartDry_json = "{\"brand\":\"SmartDry\",\"model\":\"Laundry Sensor\",\"model_id\":\"SDLS\",\"cidc\":false,\"condition\":[\"manufacturerdata\",\"=\",28,\"index\",0,\"ae01\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,8,true,false,true]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",12,8,true,false,true]},\"shake\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",23,0,false,true]},\"shake_count\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2,false,false]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,2,false,false],\"post_proc\":[\"+\",\"2847\",\"/\",1000]},\"wake\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",27,0,false,true]}}}";
-
+const char* _SmartDry_json = "{\"brand\":\"SmartDry\",\"model\":\"Laundry Sensor\",\"model_id\":\"SDLS\",\"cidc\":false,\"condition\":[\"manufacturerdata\",\"=\",28,\"index\",0,\"ae01\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,8,true,false,true]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",12,8,true,false,true]},\"shake\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,4,true,false]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,2,false,false],\"post_proc\":[\"+\",\"2847\",\"/\",1000]},\"wake\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",27,0,false,true]}}}";
 /* R""""(
 {
    "brand":"SmartDry",
@@ -15,10 +14,7 @@ const char* _SmartDry_json = "{\"brand\":\"SmartDry\",\"model\":\"Laundry Sensor
          "decoder":["value_from_hex_data", "manufacturerdata", 12, 8, true, false, true]
       },
       "shake":{
-         "decoder":["bit_static_value", "manufacturerdata", 23, 0, false, true]
-      },
-      "shake_count":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 20, 2, false, false]
+         "decoder":["value_from_hex_data", "manufacturerdata", 20, 4, true, false]
       },
       "volt":{
          "decoder":["value_from_hex_data", "manufacturerdata", 24, 2, false, false],
@@ -30,7 +26,7 @@ const char* _SmartDry_json = "{\"brand\":\"SmartDry\",\"model\":\"Laundry Sensor
    }
 })"""";*/
 
-const char* _SmartDry_json_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"},\"shake\":{\"unit\":\"status\",\"name\":\"shake\"},\"shake_count\":{\"unit\":\"int\",\"name\":\"shake count\"},\"volt\":{\"unit\":\"V\",\"name\":\"voltage\"},\"wake\":{\"unit\":\"status\",\"name\":\"wake\"}}}";
+const char* _SmartDry_json_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"},\"shake\":{\"unit\":\"int\",\"name\":\"shake\"},\"volt\":{\"unit\":\"V\",\"name\":\"voltage\"},\"wake\":{\"unit\":\"status\",\"name\":\"wake\"}}}";
 /*R""""(
 {
    "properties":{
@@ -43,12 +39,8 @@ const char* _SmartDry_json_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\"
          "name":"humidity"
       },
       "shake":{
-         "unit":"status",
-         "name":"shake"
-      },
-      "shake_count":{
          "unit":"int",
-         "name":"shake count"
+         "name":"shake"
       },
       "volt":{
          "unit":"V",
