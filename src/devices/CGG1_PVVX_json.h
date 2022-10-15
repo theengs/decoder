c2b78f558dc87314628af5fb79f6912141098d06
Charles <hallard04@free.fr>
2022-10-15 04:19:54 +0200

    fixed model

--- src/devices/CGG1_PVVX_json.h
+++ src/devices/CGG1_PVVX_json.h
@@ -1,6 +1,6 @@
 #include "common_props.h"
 
-const char* _CGG1_PVVX_json = "{\"brand\":\"Qingping\",\"model\":\"Round TH v1\",\"model_id\":\"CGG1_PVVX\",\"condition\":[\"servicedata\",\"=\",30,\"index\",6,\"342d58\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",12,4,true],\"post_proc\":[\"/\",100]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",16,4,true],\"post_proc\":[\"/\",100]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,2,false]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",1000]}}}";
+const char* _CGG1_PVVX_json = "{\"brand\":\"Qingping\",\"model\":\"CGG1\",\"model_id\":\"CGG1_PVVX\",\"condition\":[\"servicedata\",\"=\",30,\"index\",6,\"342d58\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",12,4,true],\"post_proc\":[\"/\",100]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",16,4,true],\"post_proc\":[\"/\",100]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,2,false]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",1000]}}}";
 
 /* R""""(
 {
