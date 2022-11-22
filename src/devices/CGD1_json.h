938a1524ffbb5dad19c0ebed4813d1d63b93bad7
DigiH <github@digih.net>
2022-11-22 16:08:16 +0100

    CGD1 battery
    Added CGD1 battery property

--- src/devices/CGD1_json.h
+++ src/devices/CGD1_json.h
@@ -1,6 +1,6 @@
 #include "common_props.h"
 
-const char* _CGD1_json = "{\"brand\":\"ClearGrass\",\"model\":\"Alarm Clock\",\"model_id\":\"CGD1\",\"condition\":[\"servicedata\",\"=\",34,\"index\",2,\"0c\",\"&\",\"uuid\",\"index\",0,\"fdcd\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,4,true,false],\"post_proc\":[\"/\",10]}}}";
+const char* _CGD1_json = "{\"brand\":\"ClearGrass\",\"model\":\"Alarm Clock\",\"model_id\":\"CGD1\",\"condition\":[\"servicedata\",\"=\",34,\"index\",2,\"0c\",\"&\",\"uuid\",\"index\",0,\"fdcd\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,4,true,false],\"post_proc\":[\"/\",10]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",32,2,false,false],\"post_proc\":[\"&\",127]}}}";
 
 /*R""""(
 {
@@ -10,14 +10,18 @@ const char* _CGD1_json = "{\"brand\":\"ClearGrass\",\"model\":\"Alarm Clock\",\"
    "condition":["servicedata", "=", 34, "index", 2, "0c", "&", "uuid", "index", 0, "fdcd"],
    "properties":{
       "tempc":{
-         "decoder":["value_from_hex_data", "servicedata", 20, 4, true],
+         "decoder":["value_from_hex_data", "servicedata", 20, 4, true, true],
          "post_proc":["/", 10]
       },
       "hum":{
          "decoder":["value_from_hex_data", "servicedata", 24, 4, true, false],
          "post_proc":["/", 10]
+      },
+      "batt":{
+         "decoder":["value_from_hex_data", "servicedata", 32, 2, false, false],
+         "post_proc":["&", 127]
       }
    }
 })"""";*/
 
-const char* _CGD1_json_props = _common_TH_props;
+const char* _CGD1_json_props = _common_BTH_props;
