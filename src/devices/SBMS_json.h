5e9e8df0afd7721cabf82b11b50f19ffba6e9d77
DigiH <github@digih.net>
2023-01-30 16:44:00 +0100

    SwitchBot adjustments

--- src/devices/SBMS_json.h
+++ src/devices/SBMS_json.h
@@ -1,11 +1,11 @@
-const char* _SBMS_json = "{\"brand\":\"SwitchBot\",\"model\":\"Motion Sensor\",\"model_id\":\"W110150X\",\"tag\":\"0406\",\"condition\":[\"servicedata\",\"=\",12,\"index\",0,\"73\",\"&\",\"uuid\",\"index\",0,\"0d00\"],\"properties\":{\"movement\":{\"decoder\":[\"bit_static_value\",\"servicedata\",2,2,false,true]},\"led\":{\"decoder\":[\"bit_static_value\",\"servicedata\",10,1,false,true]},\"scopetested\":{\"condition\":[\"servicedata\",2,\"bit\",3,0],\"decoder\":[\"static_value\",false]},\"sensingdistance\":{\"condition\":[\"servicedata\",11,\"bit\",3,0,\"&\",\"servicedata\",11,\"bit\",2,0],\"decoder\":[\"static_value\",\"long\"]},\"_sensingdistance\":{\"condition\":[\"servicedata\",11,\"bit\",3,0,\"&\",\"servicedata\",11,\"bit\",2,1],\"decoder\":[\"static_value\",\"middle\"]},\"__sensingdistance\":{\"condition\":[\"servicedata\",11,\"bit\",3,1,\"&\",\"servicedata\",11,\"bit\",2,0],\"decoder\":[\"static_value\",\"short\"]},\"lightlevel\":{\"condition\":[\"servicedata\",11,\"bit\",1,0,\"&\",\"servicedata\",11,\"bit\",0,1],\"decoder\":[\"static_value\",\"dark\"]},\"_lightlevel\":{\"condition\":[\"servicedata\",11,\"bit\",1,1,\"&\",\"servicedata\",11,\"bit\",0,0],\"decoder\":[\"static_value\",\"bright\"]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false],\"post_proc\":[\"&\",127]}}}";
+const char* _SBMS_json = "{\"brand\":\"SwitchBot\",\"model\":\"Motion Sensor\",\"model_id\":\"W110150X\",\"tag\":\"0406\",\"condition\":[\"uuid\",\"index\",0,\"0d00\",\"|\",\"uuid\",\"index\",0,\"fd3d\",\"&\",\"servicedata\",\"=\",12,\"index\",0,\"73\"],\"properties\":{\"movement\":{\"decoder\":[\"bit_static_value\",\"servicedata\",2,2,false,true]},\"led\":{\"decoder\":[\"bit_static_value\",\"servicedata\",10,1,false,true]},\"scopetested\":{\"condition\":[\"servicedata\",2,\"bit\",3,0],\"decoder\":[\"static_value\",false]},\"sensingdistance\":{\"condition\":[\"servicedata\",11,\"bit\",3,0,\"&\",\"servicedata\",11,\"bit\",2,0],\"decoder\":[\"static_value\",\"long\"]},\"_sensingdistance\":{\"condition\":[\"servicedata\",11,\"bit\",3,0,\"&\",\"servicedata\",11,\"bit\",2,1],\"decoder\":[\"static_value\",\"middle\"]},\"__sensingdistance\":{\"condition\":[\"servicedata\",11,\"bit\",3,1,\"&\",\"servicedata\",11,\"bit\",2,0],\"decoder\":[\"static_value\",\"short\"]},\"lightlevel\":{\"decoder\":[\"bit_static_value\",\"servicedata\",11,1,\"dark\",\"bright\"]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false],\"post_proc\":[\"&\",127]}}}";
 /*R""""(
 {
    "brand":"SwitchBot",
    "model":"Motion Sensor",
    "model_id":"W110150X",
    "tag":"0406",
-   "condition":["servicedata", "=", 12, "index", 0, "73", "&", "uuid", "index", 0, "0d00"],
+   "condition":["uuid", "index", 0, "0d00", "|", "uuid", "index", 0, "fd3d", "&", "servicedata", "=", 12, "index", 0, "73"],
    "properties":{
       "movement":{
          "decoder":["bit_static_value", "servicedata", 2, 2, false, true]
@@ -30,12 +30,7 @@ const char* _SBMS_json = "{\"brand\":\"SwitchBot\",\"model\":\"Motion Sensor\",\
          "decoder":["static_value", "short"]
       },
       "lightlevel":{
-         "condition":["servicedata", 11, "bit", 1, 0, "&","servicedata", 11, "bit", 0, 1],
-         "decoder":["static_value", "dark"]
-      },
-      "_lightlevel":{
-         "condition":["servicedata", 11, "bit", 1, 1, "&","servicedata", 11, "bit", 0, 0],
-         "decoder":["static_value", "bright"]
+         "decoder":["bit_static_value", "servicedata", 11, 1, "dark", "bright"]
       },
       "batt":{
          "decoder":["value_from_hex_data", "servicedata", 4, 2, false, false],
