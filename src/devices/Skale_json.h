c290efc9c4b383c6c49689e4f27728325924b32a
DigiH <github@digih.net>
2023-02-20 23:07:13 +0100

    Skale negative weight fix (#291)
    Skale negative weight fix with possible negative weight through Tare
    
    Co-authored-by: Cadsters <26138119+Cadsters@users.noreply.github.com>

--- src/devices/Skale_json.h
+++ src/devices/Skale_json.h
@@ -1,4 +1,4 @@
-const char* _Skale_json = "{\"brand\":\"Atomax\",\"model\":\"Skale I/II\",\"model_id\":\"SKALE\",\"tag\":\"0501\",\"condition\":[\"manufacturerdata\",\"=\",12,\"index\",0,\"ef81\"],\"properties\":{\"weight\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,4,true,false],\"post_proc\":[\"/\",10]}}}";
+const char* _Skale_json = "{\"brand\":\"Atomax\",\"model\":\"Skale I/II\",\"model_id\":\"SKALE\",\"tag\":\"0501\",\"condition\":[\"manufacturerdata\",\"=\",12,\"index\",0,\"ef81\"],\"properties\":{\"weight\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",4,4,true,true],\"post_proc\":[\"/\",10]}}}";
 /*R""""(
 {
    "brand":"Atomax",
@@ -8,7 +8,7 @@ const char* _Skale_json = "{\"brand\":\"Atomax\",\"model\":\"Skale I/II\",\"mode
    "condition":["manufacturerdata", "=", 12, "index", 0, "ef81"],
    "properties":{
       "weight":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 4, 4, true, false],
+         "decoder":["value_from_hex_data", "manufacturerdata", 4, 4, true, true],
          "post_proc":["/", 10]
       }
    }
