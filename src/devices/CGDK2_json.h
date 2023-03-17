c912bd65ed4eab332ad49c8b2f4efa9d0c7aea9b
Tomasz Machalski <toomyem@gmail.com>
2023-03-17 13:14:44 +0100

    Added CGDK2 PVVX and ATC1441 support (#313)

--- src/devices/CGDK2_json.h
+++ src/devices/CGDK2_json.h
@@ -1,6 +1,6 @@
 #include "common_props.h"
 
-const char* _CGDK2_json = "{\"brand\":\"Qingping\",\"model\":\"TH Lite\",\"model_id\":\"CGDK2\",\"tag\":\"01\",\"condition\":[\"servicedata\",\"=\",34,\"index\",2,\"10\",\"&\",\"uuid\",\"index\",0,\"fdcd\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,4,true,false],\"post_proc\":[\"/\",10]}}}";
+const char* _CGDK2_json_STOCK = "{\"brand\":\"Qingping\",\"model\":\"TH Lite\",\"model_id\":\"CGDK2\",\"tag\":\"01\",\"condition\":[\"servicedata\",\"=\",34,\"index\",2,\"10\",\"&\",\"uuid\",\"index\",0,\"fdcd\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,4,true,false],\"post_proc\":[\"/\",10]}}}";
 /*R""""(
 {
    "brand":"Qingping",
@@ -20,4 +20,59 @@ const char* _CGDK2_json = "{\"brand\":\"Qingping\",\"model\":\"TH Lite\",\"model
    }
 })"""";*/
 
-const char* _CGDK2_json_props = _common_TH_props;
+// ATC1441
+const char* _CGDK2_json_ATC1441 = "{\"brand\":\"ClearGrass/Qingping\",\"model\":\"TH Lite\",\"model_id\":\"CGDK2_ATC1441\",\"tag\":\"0102\",\"condition\":[\"servicedata\",\"=\",26,\"&\",\"uuid\",\"index\",0,\"181a\",\"&\",\"name\",\"index\",0,\"CGDK\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",12,4,false],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",16,2,false]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",18,2,false]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,false],\"post_proc\":[\"/\",1000]}}}";
+/* R""""(
+{
+   "brand":"ClearGrass/Qingping",
+   "model":"TH Lite",
+   "model_id":"CGDK2_ATC1441",
+   "tag":"0102",
+   "condition":["servicedata", "=", 26, "&", "uuid", "index", 0, "181a", "&", "name", "index", 0, "CGDK"],
+   "properties":{
+      "tempc":{
+         "decoder":["value_from_hex_data", "servicedata", 12, 4, false],
+         "post_proc":["/", 10]
+      },
+      "hum":{
+         "decoder":["value_from_hex_data", "servicedata", 16, 2, false]
+      },
+      "batt":{
+         "decoder":["value_from_hex_data", "servicedata", 18, 2, false]
+      },
+      "volt":{
+         "decoder":["value_from_hex_data", "servicedata", 20, 4, false],
+         "post_proc":["/", 1000]
+      }
+   }
+})"""";*/
+
+// PVVX
+const char* _CGDK2_json_PVVX = "{\"brand\":\"ClearGrass/Qingping\",\"model\":\"TH Lite\",\"model_id\":\"CGDK2_PVVX\",\"tag\":\"0102\",\"condition\":[\"servicedata\",\"=\",30,\"&\",\"uuid\",\"index\",0,\"181a\",\"&\",\"name\",\"index\",0,\"CGDK\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",12,4,true],\"post_proc\":[\"/\",100]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",16,4,true],\"post_proc\":[\"/\",100]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",24,2,false]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,4,true],\"post_proc\":[\"/\",1000]}}}";
+/* R""""(
+{
+   "brand":"ClearGrass/Qingping",
+   "model":"TH Lite",
+   "model_id":"CGDK2_PVVX",
+   "tag":"0102",
+   "condition":["servicedata", "=", 30, "&", "uuid", "index", 0, "181a", "&", "name", "index", 0, "CGDK"],
+   "properties":{
+      "tempc":{
+         "decoder":["value_from_hex_data", "servicedata", 12, 4, true],
+         "post_proc":["/", 100]
+      },
+      "hum":{
+         "decoder":["value_from_hex_data", "servicedata", 16, 4, true],
+         "post_proc":["/", 100]
+      },
+      "batt":{
+         "decoder":["value_from_hex_data", "servicedata", 24, 2, false]
+      },
+      "volt":{
+         "decoder":["value_from_hex_data", "servicedata", 20, 4, true],
+         "post_proc":["/", 1000]
+      }
+   }
+})"""";*/
+
+const char* _CGDK2_json_props = _common_BVTH_props;
