7bf5c4f7e52fae911842e6ea3c4a76cecf508640
DigiH <github@digih.net>
2023-04-12 15:15:09 +0200

    m_docMax Test (#329)
    m_docMax Test to see possible changed behaviour for
    
    https://github.com/theengs/gateway/issues/124

--- src/decoder.h
+++ src/decoder.h
@@ -140,7 +140,7 @@ private:
                                const char* dev_name, const char* svc_uuid, const char* mac_id);
   std::string sanitizeJsonKey(const char* key_in);
 
-  size_t m_docMax = 12000;
+  size_t m_docMax = 13000;
   size_t m_minSvcDataLen = 20;
   size_t m_minMfgDataLen = 16;
 };
