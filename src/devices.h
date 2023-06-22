94f72ce7bb6d6e747e72234a36122c0455b7627b
Koen Vervloesem <koen@vervloesem.eu>
2023-06-22 12:59:29 +0200

    Add decoder for encrypted advertisements of ShellyBLU Button1 (#370)

--- src/devices.h
+++ src/devices.h
@@ -89,6 +89,7 @@
 #include "devices/APPLE_json.h"
 #include "devices/ServiceData_json.h"
 #include "devices/SBBT_002C_json.h"
+#include "devices/SBBT_002C_encrypted_json.h"
 
 
 const char* _devices[][2] = {
@@ -174,4 +175,5 @@ const char* _devices[][2] = {
     {_APPLE_json_at, _APPLE_json_props},
     {_ServiceData_json, _ServiceData_json_props},
     {_SBBT_002C_json, _SBBT_002C_json_props},
+    {_SBBT_002C_encrypted_json, _SBBT_002C_encrypted_json_props},
 };
