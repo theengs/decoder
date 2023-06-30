a7ac045c459f34471cb7ff5b14a2d7d5ef4d95e4
Koen Vervloesem <koen@vervloesem.eu>
2023-06-30 18:09:55 +0200

    Fix check_decoder script (#383)

--- scripts/check_decoder.py
+++ scripts/check_decoder.py
@@ -1,9 +1,31 @@
+"""Helper script to check validity of JSON strings in a decoder.
+
+Run this on any .h file in src/devices/ to check the decoder file.
+"""
 import json
 import re
 import sys
+from pathlib import Path
 
 
-def fix_json_strings(decoder_source):
+def normalize_json_string(json_string: str) -> str:
+    """Normalize a JSON string by escaping quotes and removing whitespace."""
+    in_quotes = False
+    normalized_json_string = ""
+
+    for character in json_string:
+        if character == '"':
+            in_quotes = not in_quotes
+
+        if not in_quotes and character in [" ", "\n", "\t"]:
+            continue
+
+        normalized_json_string += character
+
+    return normalized_json_string.replace('"', '\\"')
+
+
+def fix_json_strings(decoder_source: str) -> str:
     """Check whether declaration in decoder source file conforms to raw string.
 
     This returns the complete decoder source code with the JSON declaration
@@ -19,41 +41,48 @@ def fix_json_strings(decoder_source):
 
     for declaration, json_string, raw_string in code_structures:
 
-        # Parse the raw JSON string and convert it back to unindented string
-        json_data = json.loads(raw_string)
-        normalized_data = json.dumps(
-            json_data, separators=(",", ":"), ensure_ascii=False, indent=None
-        )
+        # Parse the raw JSON string.
+        # If it's not valid JSON, this raises a json.decoder.JSONDecodeError.
+        _ = json.loads(raw_string)
 
-        # Escape quotes
-        normalized_data = normalized_data.replace('"', '\\"')
+        # Normalize JSON string
+        # Don't use json.dumps because this changes floats to scientific notation.
+        normalized_data = normalize_json_string(raw_string)
 
         # Replace the original JSON string by the normalized raw string
-        decoder_source = decoder_source.replace(declaration + "\"" + json_string, declaration + "\"" + normalized_data)
+        decoder_source = decoder_source.replace(
+            declaration + '"' + json_string,
+            declaration + '"' + normalized_data,
+        )
 
     return decoder_source
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Please provide a filename as command-line argument.")
-        sys.exit()
-
-    filename = sys.argv[1]
-
-    with open(filename, "r") as file:
-        content = file.read()
-
-    try:
-        new_decoder_source = fix_json_strings(content)
-    except json.decoder.JSONDecodeError as error:
-        print(f"Invalid JSON at: {error.doc[:error.pos]}")
-        print(f"Error message: {error.msg}")
+        print("Please provide one or more filenames as command-line arguments.")
         sys.exit(1)
 
-    # Write the result back to the file
-    if new_decoder_source != content:
-        with open(filename, "w") as file:
-            file.write(new_decoder_source)
-        print(f"Fixing JSON string in decoder {filename}...")
+    has_error = False
+
+    for filename in sys.argv[1:]:
+        with Path.open(filename) as file:
+            content = file.read()
+
+        try:
+            new_decoder_source = fix_json_strings(content)
+
+            # Write the result back to the file
+            if new_decoder_source != content:
+                with Path.open(filename, "w") as file:
+                    file.write(new_decoder_source)
+                print(f"Fixing JSON string in decoder {filename}...")
+                has_error = True
+        except json.decoder.JSONDecodeError as error:
+            print(f"Can't automatically fix decoder {filename}")
+            print(f"Invalid JSON at: {error.doc[:error.pos]}")
+            print(f"Error message: {error.msg}")
+            has_error = True
+
+    if has_error:
         sys.exit(1)
