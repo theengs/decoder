6afa7120875426fcecf25f2331876db8aa6fe5b2
Koen Vervloesem <koen@vervloesem.eu>
2023-08-21 11:37:54 +0200

    Fix ABN07 decoder for firmware 4.0.1

--- src/devices/ABN07_json.h
+++ src/devices/ABN07_json.h
@@ -1,4 +1,4 @@
-const char* _ABN07_json = "{\"brand\":\"April Brother\",\"model\":\"N07\",\"model_id\":\"ABN07\",\"tag\":\"0102\",\"condition\":[\"servicedata\",\"=\",22,\"index\",0,\"40\",\"&\",\"uuid\",\"index\",0,\"fcd2\",\"&\",\"name\",\"index\",0,\"asensor_\"],\"properties\":{\"batt\":{\"condition\":[\"servicedata\",2,\"01\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false]},\"tempc\":{\"condition\":[\"servicedata\",6,\"02\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",8,4,true,true],\"post_proc\":[\"/\",100]},\"hum\":{\"condition\":[\"servicedata\",12,\"03\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",14,4,true,false],\"post_proc\":[\"/\",100]},\"packet\":{\"condition\":[\"servicedata\",18,\"00\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",20,2,false,false]}}}";
+const char* _ABN07_json = "{\"brand\":\"April Brother\",\"model\":\"N07\",\"model_id\":\"ABN07\",\"tag\":\"0102\",\"condition\":[\"servicedata\",\"=\",22,\"index\",0,\"40\",\"&\",\"uuid\",\"index\",0,\"fcd2\",\"&\",\"name\",\"index\",0,\"asensor_\"],\"properties\":{\"packet\":{\"condition\":[\"servicedata\",2,\"00\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false]},\"batt\":{\"condition\":[\"servicedata\",6,\"01\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",8,2,false,false]},\"tempc\":{\"condition\":[\"servicedata\",10,\"02\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",12,4,true,true],\"post_proc\":[\"/\",100]},\"hum\":{\"condition\":[\"servicedata\",16,\"03\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",18,4,true,false],\"post_proc\":[\"/\",100]}}}";
 /* R""""(
 {
    "brand":"April Brother",
@@ -7,31 +7,35 @@ const char* _ABN07_json = "{\"brand\":\"April Brother\",\"model\":\"N07\",\"mode
    "tag":"0102",
    "condition":["servicedata", "=", 22, "index", 0, "40", "&", "uuid", "index", 0, "fcd2", "&", "name", "index", 0, "asensor_"],
    "properties":{
-      "batt":{
-         "condition":["servicedata", 2, "01"],
+      "packet":{
+         "condition":["servicedata", 2, "00"],
          "decoder":["value_from_hex_data", "servicedata", 4, 2, false, false]
       },
+      "batt":{
+         "condition":["servicedata", 6, "01"],
+         "decoder":["value_from_hex_data", "servicedata", 8, 2, false, false]
+      },
       "tempc":{
-         "condition":["servicedata", 6, "02"],
-         "decoder":["value_from_hex_data", "servicedata", 8, 4, true, true],
+         "condition":["servicedata", 10, "02"],
+         "decoder":["value_from_hex_data", "servicedata", 12, 4, true, true],
          "post_proc":["/", 100]
       },
       "hum":{
-         "condition":["servicedata", 12, "03"],
-         "decoder":["value_from_hex_data", "servicedata", 14, 4, true, false],
+         "condition":["servicedata", 16, "03"],
+         "decoder":["value_from_hex_data", "servicedata", 18, 4, true, false],
          "post_proc":["/", 100]
-      },
-      "packet":{
-         "condition":["servicedata", 18, "00"],
-         "decoder":["value_from_hex_data", "servicedata", 20, 2, false, false]
       }
    }
 })"""";*/
 
-const char* _ABN07_json_props = "{\"properties\":{\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"},\"packet\":{\"unit\":\"int\",\"name\":\"packet id\"}}}";
+const char* _ABN07_json_props = "{\"properties\":{\"packet\":{\"unit\":\"int\",\"name\":\"packet id\"},\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"}}}";
 /*R""""(
 {
    "properties":{
+      "packet":{
+         "unit":"int",
+         "name":"packet id"
+      },
       "batt":{
          "unit":"%",
          "name":"battery"
@@ -43,10 +47,6 @@ const char* _ABN07_json_props = "{\"properties\":{\"batt\":{\"unit\":\"%\",\"nam
       "hum":{
          "unit":"%",
          "name":"humidity"
-      },
-      "packet":{
-         "unit":"int",
-         "name":"packet id"
       }
    }
 })"""";*/
