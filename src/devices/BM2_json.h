6007cf448567a90026ec0514da21f2b18bf8123e
DigiH <github@digih.net>
2023-09-24 14:16:54 +0200

    BM2 Battery Monitor Tracker addition (#418)

--- src/devices/BM2_json.h
+++ src/devices/BM2_json.h
@@ -1,26 +1,33 @@
-const char* _BM2_json = "{\"brand\":\"GENERIC\",\"model\":\"BM2 Battery Monitor\",\"model_id\":\"BM2\",\"tag\":\"0802\",\"condition\":[\"name\",\"index\",0,\"ZX-1689\",\"|\",\"name\",\"index\",0,\"Battery Monitor\",\"|\",\"name\",\"index\",0,\"Li Battery Monitor\",\"&\",\"manufacturerdata\",\"=\",50],\"properties\":{\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]}}}";
+const char* _BM2_json = "{\"brand\":\"GENERIC\",\"model\":\"BM2 Battery Monitor\",\"model_id\":\"BM2\",\"tag\":\"080a\",\"condition\":[\"name\",\"index\",0,\"ZX-1689\",\"|\",\"name\",\"index\",0,\"Battery Monitor\",\"|\",\"name\",\"index\",0,\"Li Battery Monitor\",\"&\",\"manufacturerdata\",\"=\",50],\"properties\":{\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"device\":{\"decoder\":[\"static_value\",\"BM2 Tracker\"]}}}";
 
 /*R""""(
 {
    "brand":"GENERIC",
    "model":"BM2 Battery Monitor",
    "model_id":"BM2",
-   "tag":"0802",
+   "tag":"080a",
    "condition":["name", "index", 0, "ZX-1689", "|", "name", "index", 0, "Battery Monitor", "|", "name", "index", 0, "Li Battery Monitor", "&","manufacturerdata", "=", 50],
    "properties":{
       "batt":{
          "decoder":["value_from_hex_data", "manufacturerdata", 48, 2, false]
+      },
+      "device":{
+         "decoder":["static_value", "BM2 Tracker"]
       }
    }
 })"""";*/
 
-const char* _BM2_json_props = "{\"properties\":{\"batt\":{\"unit\":\"%\",\"name\":\"battery\"}}}";
+const char* _BM2_json_props = "{\"properties\":{\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"device\":{\"unit\":\"string\",\"name\":\"tracker device\"}}}";
 /*R""""(
 {
    "properties":{
       "batt":{
          "unit":"%",
          "name":"battery"
+      },
+      "device":{
+         "unit":"string",
+         "name":"tracker device"
       }
    }
 })"""";*/
