ef3d33d24c189af7252ed10e582077e40feb75fa
DigiH <github@digih.net>
2023-10-20 09:02:22 +0200

    BM2 model fix
    BM2 model fix for connection recognition in OpenMQTTGateway

--- src/devices/BM2_json.h
+++ src/devices/BM2_json.h
@@ -1,9 +1,9 @@
-const char* _BM2_json = "{\"brand\":\"GENERIC\",\"model\":\"Battery Monitor\",\"model_id\":\"BM2\",\"tag\":\"0808\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c000215655f83caae16a10a702e31f30d58dd82\"],\"properties\":{\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"device\":{\"decoder\":[\"static_value\",\"BM2 Tracker\"]}}}";
+const char* _BM2_json = "{\"brand\":\"GENERIC\",\"model\":\"BM2 Battery Monitor\",\"model_id\":\"BM2\",\"tag\":\"0808\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c000215655f83caae16a10a702e31f30d58dd82\"],\"properties\":{\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",48,2,false]},\"device\":{\"decoder\":[\"static_value\",\"BM2 Tracker\"]}}}";
 
 /*R""""(
 {
    "brand":"GENERIC",
-   "model":"Battery Monitor",
+   "model":"BM2 Battery Monitor",
    "model_id":"BM2",
    "tag":"0808",
    "condition":["manufacturerdata", "=", 50, "index", 0, "4c000215655f83caae16a10a702e31f30d58dd82"],
