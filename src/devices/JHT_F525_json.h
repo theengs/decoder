72fbf0f9f3db70d85bd51088034ec8ec92208dbe
DigiH <github@digih.net>
2024-01-10 16:50:45 +0100

    Jaalee F525 fix
    Jaalee F525 calculation fix for temperature and humidity

--- src/devices/JHT_F525_json.h
+++ src/devices/JHT_F525_json.h
@@ -1,6 +1,6 @@
 #include "common_props.h"
 
-const char* _JHT_F525_json = "{\"brand\":\"Jaalee\",\"model\":\"TH sensor\",\"model_id\":\"F525\",\"tag\":\"0102\",\"condition\":[\"manufacturerdata\",\"=\",52,\"&\",\"uuid\",\"contain\",\"f525\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",40,4,false],\"post_proc\":[\"*\",175.72,\"/\",65536,\"-\",46.85]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",44,4,false,false],\"post_proc\":[\"*\",125.0,\"/\",65536,\"-\",6]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",50,2,false,false]}}}";
+const char* _JHT_F525_json = "{\"brand\":\"Jaalee\",\"model\":\"TH sensor\",\"model_id\":\"F525\",\"tag\":\"0102\",\"condition\":[\"manufacturerdata\",\"=\",52,\"&\",\"uuid\",\"contain\",\"f525\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",40,4,false],\"post_proc\":[\"*\",175,\"/\",65535,\"-\",45]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",44,4,false,false],\"post_proc\":[\"*\",100,\"/\",65535]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",50,2,false,false]}}}";
 /* R""""(
 {
    "brand":"Jaalee",
@@ -11,11 +11,11 @@ const char* _JHT_F525_json = "{\"brand\":\"Jaalee\",\"model\":\"TH sensor\",\"mo
    "properties":{
       "tempc":{
          "decoder":["value_from_hex_data", "manufacturerdata", 40, 4, false],
-         "post_proc":["*", 175.72, "/", 65536, "-", 46.85]
+         "post_proc":["*", 175, "/", 65535, "-", 45]
       },
       "hum":{
          "decoder":["value_from_hex_data", "manufacturerdata", 44, 4, false, false],
-         "post_proc":["*", 125.0, "/", 65536, "-", 6]
+         "post_proc":["*", 100, "/", 65535]
       },
       "batt":{
          "decoder":["value_from_hex_data", "manufacturerdata", 50, 2, false, false]
