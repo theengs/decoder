baa765ea3d888e183072abf42febc08b2f91285f
John Garner <jgbreezer@gmail.com>
2024-01-15 20:03:16 +0000

    TicWatchGTH_json.h: remove 0x prefix on tag

--- src/devices/TicWatchGTH_json.h
+++ src/devices/TicWatchGTH_json.h
@@ -4,7 +4,7 @@ R""""(
    "model":"GTH/GTH Pro",
    "model_id":"TICWATCHGTH",
    "condition":["name", "index", 0, "TicWatch GTH"],
-   "tag": "0x0f0b",
+   "tag": "0f0b",
    "properties":{
       "mac":{
          "decoder":["value_from_hex_data", "manufacturerdata", 4]
