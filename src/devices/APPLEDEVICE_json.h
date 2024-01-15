bd1f2b7ae52b2f71b2246eaa37976a052e7380f5
DigiH <github@digih.net>
2024-01-15 13:16:11 +0100

    Apple iPhone/iPad extension

--- src/devices/APPLEDEVICE_json.h
+++ src/devices/APPLEDEVICE_json.h
@@ -1,4 +1,4 @@
-const char* _APPLEDEVICE_json = "{\"brand\":\"Apple\",\"model\":\"Apple iPhone/iPad\",\"model_id\":\"APPLEDEVICE\",\"tag\":\"1018\",\"condition\":[\"manufacturerdata\",\">=\",8,\"index\",0,\"4c0010\"],\"properties\":{\"unlocked\":{\"condition\":[\"manufacturerdata\",9,\"b\"],\"decoder\":[\"static_value\",true]},\"_unlocked\":{\"condition\":[\"manufacturerdata\",9,\"!\",\"b\"],\"decoder\":[\"static_value\",false]}}}";
+const char* _APPLEDEVICE_json = "{\"brand\":\"Apple\",\"model\":\"Apple iPhone/iPad\",\"model_id\":\"APPLEDEVICE\",\"tag\":\"1018\",\"condition\":[\"manufacturerdata\",\">=\",8,\"index\",0,\"4c0010\"],\"properties\":{\"unlocked\":{\"condition\":[\"manufacturerdata\",9,\"b\",\"|\",\"manufacturerdata\",9,\"7\",\"|\",\"manufacturerdata\",9,\"9\"],\"decoder\":[\"static_value\",true]},\"_unlocked\":{\"condition\":[\"manufacturerdata\",9,\"!\",\"b\",\"&\",\"manufacturerdata\",9,\"!\",\"7\",\"&\",\"manufacturerdata\",9,\"!\",\"9\"],\"decoder\":[\"static_value\",false]}}}";
 /*R""""(
 {
    "brand":"Apple",
@@ -8,11 +8,11 @@ const char* _APPLEDEVICE_json = "{\"brand\":\"Apple\",\"model\":\"Apple iPhone/i
    "condition":["manufacturerdata", ">=", 8, "index", 0, "4c0010"],
    "properties":{
       "unlocked":{
-         "condition":["manufacturerdata", 9, "b"],
+         "condition":["manufacturerdata", 9, "b", "|", "manufacturerdata", 9, "7", "|", "manufacturerdata", 9, "9"],
          "decoder":["static_value", true]
       },
       "_unlocked":{
-         "condition":["manufacturerdata", 9, "!", "b"],
+         "condition":["manufacturerdata", 9, "!", "b", "&", "manufacturerdata", 9, "!", "7", "&", "manufacturerdata", 9, "!", "9"],
          "decoder":["static_value", false]
       }
    }
