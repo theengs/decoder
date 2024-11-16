160d2fccd4473cd36636e8797bf1f2a19fbe9c81
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2024-11-16 15:55:09 +0100

    Detect battery level for ThermoPro TP357, TP358, TP359 and TP393

--- src/devices/TPTH_json.h
+++ src/devices/TPTH_json.h
@@ -1,6 +1,6 @@
 #include "common_props.h"
 
-const char* _TPTH_json = "{\"brand\":\"ThermoPro\",\"model\":\"TH Sensor\",\"model_id\":\"TP35X/393\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"TP357\",\"|\",\"name\",\"index\",0,\"TP358\",\"|\",\"name\",\"index\",0,\"TP359\",\"|\",\"name\",\"index\",0,\"TP393\",\"&\",\"manufacturerdata\",\">=\",12,\"index\",0,\"c2\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",2,4,true,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",6,2,false,false]}}}";
+const char* _TPTH_json = "{\"brand\":\"ThermoPro\",\"model\":\"TH Sensor\",\"model_id\":\"TP35X/393\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"TP357\",\"|\",\"name\",\"index\",0,\"TP358\",\"|\",\"name\",\"index\",0,\"TP359\",\"|\",\"name\",\"index\",0,\"TP393\",\"&\",\"manufacturerdata\",\">=\",12,\"index\",0,\"c2\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",2,4,true,true],\"post_proc\":[\"/\",10]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",6,2,false,false]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",9,1,false,false],\"post_proc\":[\"*\",50,\"max\",100]}}}";
 /*R""""(
 {
    "brand":"ThermoPro",
@@ -15,8 +15,12 @@ const char* _TPTH_json = "{\"brand\":\"ThermoPro\",\"model\":\"TH Sensor\",\"mod
       },
       "hum":{
          "decoder":["value_from_hex_data", "manufacturerdata", 6, 2, false, false]
+      },
+      "batt":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 9, 1, false, false],
+         "post_proc":["*", 50, "max", 100]
       }
    }
 })"""";*/
 
-const char* _TPTH_json_props = _common_TH_props;
+const char* _TPTH_json_props = _common_BTH_props;
