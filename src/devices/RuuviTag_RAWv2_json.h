78ad6aad39d53ec7a543c075df4d8ff61cdc3ee1
DigiH <17110652+DigiH@users.noreply.github.com>
2025-03-17 15:52:01 +0100

    Ruuvi Tags v2 Invalid / not available conditions added

--- src/devices/RuuviTag_RAWv2_json.h
+++ src/devices/RuuviTag_RAWv2_json.h
@@ -1,4 +1,4 @@
-const char* _RuuviTag_RAWv2_json = "{\"brand\":\"Ruuvi\",\"model\":\"RuuviTag\",\"model_id\":\"RuuviTag_RAWv2\",\"tag\":\"0708\",\"condition\":[\"manufacturerdata\",\"=\",52,\"index\",0,\"990405\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",6,4,false,true],\"post_proc\":[\"/\",200]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,4,false,false],\"post_proc\":[\"/\",400]},\"pres\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",14,4,false,false],\"post_proc\":[\"+\",50000,\"/\",100]},\"accx\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",18,4,false,true],\"post_proc\":[\"/\",10000,\"*\",9.80665]},\"accy\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,4,false,true],\"post_proc\":[\"/\",10000,\"*\",9.80665]},\"accz\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",26,4,false,true],\"post_proc\":[\"/\",10000,\"*\",9.80665]},\"volt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",30,4,false,false],\"post_proc\":[\">\",5,\"+\",1600,\"/\",1000]},\"tx\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",30,4,false,false],\"post_proc\":[\"%\",32,\"*\",2,\"-\",40]},\"mov\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",34,2,false,false]},\"seq\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",36,4,false,false]},\"mac\":{\"decoder\":[\"mac_from_hex_data\",\"manufacturerdata\",40]}}}";
+const char* _RuuviTag_RAWv2_json = "{\"brand\":\"Ruuvi\",\"model\":\"RuuviTag\",\"model_id\":\"RuuviTag_RAWv2\",\"tag\":\"0708\",\"condition\":[\"manufacturerdata\",\"=\",52,\"index\",0,\"990405\"],\"properties\":{\"tempc\":{\"condition\":[\"manufacturerdata\",6,\"!\",\"8000\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",6,4,false,true],\"post_proc\":[\"/\",200]},\"hum\":{\"condition\":[\"manufacturerdata\",10,\"!\",\"ffff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",10,4,false,false],\"post_proc\":[\"/\",400]},\"pres\":{\"condition\":[\"manufacturerdata\",14,\"!\",\"ffff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",14,4,false,false],\"post_proc\":[\"+\",50000,\"/\",100]},\"accx\":{\"condition\":[\"manufacturerdata\",18,\"!\",\"8000\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",18,4,false,true],\"post_proc\":[\"/\",10000,\"*\",9.80665]},\"accy\":{\"condition\":[\"manufacturerdata\",22,\"!\",\"8000\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,4,false,true],\"post_proc\":[\"/\",10000,\"*\",9.80665]},\"accz\":{\"condition\":[\"manufacturerdata\",26,\"!\",\"8000\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",26,4,false,true],\"post_proc\":[\"/\",10000,\"*\",9.80665]},\"volt\":{\"condition\":[\"manufacturerdata\",30,\"!\",\"7ff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",30,4,false,false],\"post_proc\":[\">\",5,\"+\",1600,\"/\",1000]},\"tx\":{\"condition\":[\"manufacturerdata\",33,\"!\",\"f\",\"&\",\"manufacturerdata\",32,\"!\",\"1\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",30,4,false,false],\"post_proc\":[\"%\",32,\"*\",2,\"-\",40]},\"mov\":{\"condition\":[\"manufacturerdata\",34,\"!\",\"ff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",34,2,false,false]},\"seq\":{\"condition\":[\"manufacturerdata\",36,\"!\",\"ffff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",36,4,false,false]},\"mac\":{\"decoder\":[\"mac_from_hex_data\",\"manufacturerdata\",40]}}}";
 /*R""""(
 {
    "brand":"Ruuvi",
@@ -8,41 +8,51 @@ const char* _RuuviTag_RAWv2_json = "{\"brand\":\"Ruuvi\",\"model\":\"RuuviTag\",
    "condition":["manufacturerdata", "=", 52, "index", 0, "990405"],
    "properties":{
       "tempc":{
+         "condition":["manufacturerdata", 6, "!", "8000"],
          "decoder":["value_from_hex_data", "manufacturerdata", 6, 4, false, true],
          "post_proc":["/", 200]
       },
       "hum":{
+         "condition":["manufacturerdata", 10, "!", "ffff"],
          "decoder":["value_from_hex_data", "manufacturerdata", 10, 4, false, false],
          "post_proc":["/", 400]
       },
       "pres":{
+         "condition":["manufacturerdata", 14, "!", "ffff"],
          "decoder":["value_from_hex_data", "manufacturerdata", 14, 4, false, false],
          "post_proc":["+", 50000, "/", 100]
       },
       "accx":{
+         "condition":["manufacturerdata", 18, "!", "8000"],
          "decoder":["value_from_hex_data", "manufacturerdata", 18, 4, false, true],
          "post_proc":["/", 10000, "*", 9.80665]
       },
       "accy":{
+         "condition":["manufacturerdata", 22, "!", "8000"],
          "decoder":["value_from_hex_data", "manufacturerdata", 22, 4, false, true],
          "post_proc":["/", 10000, "*", 9.80665]
       },
       "accz":{
+         "condition":["manufacturerdata", 26, "!", "8000"],
          "decoder":["value_from_hex_data", "manufacturerdata", 26, 4, false, true],
          "post_proc":["/", 10000, "*", 9.80665]
       },
       "volt":{
+         "condition":["manufacturerdata", 30, "!", "7ff"],
          "decoder":["value_from_hex_data", "manufacturerdata", 30, 4, false, false],
          "post_proc":[">", 5, "+", 1600, "/", 1000]
       },
       "tx":{
+         "condition":["manufacturerdata", 33, "!", "f", "&", "manufacturerdata", 32, "!", "1"],
          "decoder":["value_from_hex_data", "manufacturerdata", 30, 4, false, false],
          "post_proc":["%", 32, "*", 2, "-", 40]
       },
       "mov":{
+         "condition":["manufacturerdata", 34, "!", "ff"],
          "decoder":["value_from_hex_data", "manufacturerdata", 34, 2, false, false]
       },
       "seq":{
+         "condition":["manufacturerdata", 36, "!", "ffff"],
          "decoder":["value_from_hex_data", "manufacturerdata", 36, 4, false, false]
       },
       "mac":{
