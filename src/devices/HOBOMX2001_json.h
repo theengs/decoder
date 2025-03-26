74ebf1ccc9d498af529e81ef215108c54f16f906
DigiH <17110652+DigiH@users.noreply.github.com>
2025-03-26 17:15:25 +0100

    Onset Hobo negative decoder Boolean

--- src/devices/HOBOMX2001_json.h
+++ src/devices/HOBOMX2001_json.h
@@ -1,4 +1,4 @@
-const char* _HOBOMX2001_json = "{\"brand\":\"Onset\",\"model\":\"Hobo Water Level Sensor\",\"model_id\":\"HOBOMX2001\",\"tag\":\"ff\",\"condition\":[\"manufacturerdata\",\"=\",44,\"index\",0,\"c500\"],\"properties\":{\"lvl_cm\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",36,8,true,false,true],\"post_proc\":[\"*\",100]}}}";
+const char* _HOBOMX2001_json = "{\"brand\":\"Onset\",\"model\":\"Hobo Water Level Sensor\",\"model_id\":\"HOBOMX2001\",\"tag\":\"ff\",\"condition\":[\"manufacturerdata\",\"=\",44,\"index\",0,\"c500\"],\"properties\":{\"lvl_cm\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",36,8,true,true,true],\"post_proc\":[\"*\",100]}}}";
 /* R""""(
 {
    "brand":"Onset",
@@ -8,7 +8,7 @@ const char* _HOBOMX2001_json = "{\"brand\":\"Onset\",\"model\":\"Hobo Water Leve
    "condition":["manufacturerdata", "=", 44, "index", 0, "c500"],
    "properties":{
       "lvl_cm":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 36, 8, true, false, true],
+         "decoder":["value_from_hex_data", "manufacturerdata", 36, 8, true, true, true],
          "post_proc":["*", 100]
       }
    }
