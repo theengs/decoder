986aa8c0f2c4024ca99b422c6b1e380a00bb0b42
Peter Lambrechtsen <peter@crypt.nz>
2025-07-30 21:33:34 +1200

    Add support for LYWSD03MMC with PVVX firmware and sending BTHome v2 BLE frames

--- src/devices/LYWSD03MMC_json.h
+++ src/devices/LYWSD03MMC_json.h
@@ -82,4 +82,109 @@ const char* _LYWSD03MMC_json_PVVX_DECR = "{\"brand\":\"Xiaomi\",\"model\":\"TH S
    }
 })"""";*/
 
+const char* _LYWSD03MMC_json_PVVX_BTHOME_1 = "{\"brand\":\"Xiaomi\",\"model\":\"TH Sensor\",\"model_id\":\"LYWSD03MMC/MJWSD05MMC_PVVX_BTHOME_1\",\"tag\":\"01\",\"condition\":[\"servicedata\",\"=\",22,\"index\",0,\"40\",\"&\",\"uuid\",\"index\",0,\"fcd2\",\"&\",\"name\",\"index\",0,\"ATC\"],\"properties\":{\"packet\":{\"condition\":[\"servicedata\",2,\"00\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false]},\"batt\":{\"condition\":[\"servicedata\",6,\"01\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",8,2,false,false]},\"tempc\":{\"condition\":[\"servicedata\",10,\"02\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",12,4,true,true],\"post_proc\":[\"/\",100]},\"hum\":{\"condition\":[\"servicedata\",16,\"03\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",18,4,true,false],\"post_proc\":[\"/\",100]}}}";
+/* R""""(
+{
+   "brand":"Xiaomi",
+   "model":"TH Sensor",
+   "model_id":"LYWSD03MMC/MJWSD05MMC_PVVX_BTHOME_1",
+   "tag":"01",
+   "condition":["servicedata", "=", 22, "index", 0, "40", "&", "uuid", "index", 0, "fcd2", "&", "name", "index", 0, "ATC"],
+   "properties":{
+      "packet":{
+         "condition":["servicedata", 2, "00"],
+         "decoder":["value_from_hex_data", "servicedata", 4, 2, false, false]
+      },
+      "batt":{
+         "condition":["servicedata", 6, "01"],
+         "decoder":["value_from_hex_data", "servicedata", 8, 2, false, false]
+      },
+      "tempc":{
+         "condition":["servicedata", 10, "02"],
+         "decoder":["value_from_hex_data", "servicedata", 12, 4, true, true],
+         "post_proc":["/", 100]
+      },
+      "hum":{
+         "condition":["servicedata", 16, "03"],
+         "decoder":["value_from_hex_data", "servicedata", 18, 4, true, false],
+         "post_proc":["/", 100]
+      }
+   }
+})"""";*/
+
+const char* _LYWSD03MMC_json_PVVX_BTHOME_2 = "{\"brand\":\"Xiaomi\",\"model\":\"TH Sensor\",\"model_id\":\"LYWSD03MMC/MJWSD05MMC_PVVX_BTHOME_2\",\"tag\":\"01\",\"condition\":[\"servicedata\",\"=\",20,\"index\",0,\"40\",\"&\",\"uuid\",\"index\",0,\"fcd2\",\"&\",\"name\",\"index\",0,\"ATC\"],\"properties\":{\"packet\":{\"condition\":[\"servicedata\",2,\"00\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false]},\"volt\":{\"condition\":[\"servicedata\",6,\"0c\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",8,4,true,false],\"post_proc\":[\"/\",1000]},\"power\":{\"condition\":[\"servicedata\",12,\"10\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",14,2,false,false]},\"opening\":{\"condition\":[\"servicedata\",16,\"11\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",18,2,false,false]}}}";
+/* R""""(
+{
+   "brand":"Xiaomi",
+   "model":"TH Sensor",
+   "model_id":"LYWSD03MMC/MJWSD05MMC_PVVX_BTHOME_2",
+   "tag":"01",
+   "condition":["servicedata", "=", 20, "index", 0, "40", "&", "uuid", "index", 0, "fcd2", "&", "name", "index", 0, "ATC"],
+   "properties":{
+      "packet":{
+         "condition":["servicedata", 2, "00"],
+         "decoder":["value_from_hex_data", "servicedata", 4, 2, false, false]
+      },
+      "volt":{
+         "condition":["servicedata", 6, "0c"],
+         "decoder":["value_from_hex_data", "servicedata", 8, 4, true, false],
+         "post_proc":["/", 1000]
+      },
+      "power":{
+         "condition":["servicedata", 12, "10"],
+         "decoder":["value_from_hex_data", "servicedata", 14, 2, false, false]
+      },
+      "opening":{
+         "condition":["servicedata", 16, "11"],
+         "decoder":["value_from_hex_data", "servicedata", 18, 2, false, false]
+      }
+   }
+})"""";*/
+
 const char* _LYWSD03MMC_json_props = _common_BVTH_props;
+
+const char* _LYWSD03MMC_BTHOME_1_json_props = "{\"properties\":{\"packet\":{\"unit\":\"int\",\"name\":\"packet id\"},\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"hum\":{\"unit\":\"%\",\"name\":\"humidity\"}}}";
+/*R""""(
+{
+   "properties":{
+      "packet":{
+         "unit":"int",
+         "name":"packet id"
+      },
+      "batt":{
+         "unit":"%",
+         "name":"battery"
+      },
+      "tempc":{
+         "unit":"°C",
+         "name":"temperature"
+      },
+      "hum":{
+         "unit":"%",
+         "name":"humidity"
+      }
+   }
+})"""";*/
+
+const char* _LYWSD03MMC_BTHOME_2_json_props = "{\"properties\":{\"packet\":{\"unit\":\"int\",\"name\":\"packet id\"},\"volt\":{\"unit\":\"V\",\"name\":\"voltage\"},\"power\":{\"unit\":\"int\",\"name\":\"power\"},\"opening\":{\"unit\":\"int\",\"name\":\"opening\"}}}";
+/*R""""(
+{
+   "properties":{
+      "packet":{
+         "unit":"int",
+         "name":"packet id"
+      },
+      "volt": {
+         "unit": "V",
+         "name": "voltage"
+      },
+      "power": {
+         "unit": "int",
+         "name": "power"
+      },
+      "opening": {
+         "unit": "int",
+         "name": "opening"
+      }
+   }
+})"""";*/
