d8c56c4b84839bb78bcf425ba8aa928a3508a54d
Tyler Ward <twardnw@gmail.com>
2025-08-24 13:42:38 -0700

    fix: data is zero-indexed, P02B will not send negative temp

--- src/devices/IBS_P02B_json.h
+++ src/devices/IBS_P02B_json.h
@@ -1,4 +1,4 @@
-const char* _IBS_P02B_json = "{\"brand\":\"Inkbird\",\"model\":\"Pool Thermometer\",\"model_id\":\"IBS-P02B\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"IBS-P02B\",\"&\",\"manufacturerdata\",\"=\",36],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",13,4,true],\"post_proc\":[\"/\",10]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2]},\"lowBat\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",26,0,\"false\",\"true\"]}}}";
+const char* _IBS_P02B_json = "{\"brand\":\"Inkbird\",\"model\":\"Pool Thermometer\",\"model_id\":\"IBS-P02B\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"IBS-P02B\",\"&\",\"manufacturerdata\",\"=\",36],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",12,2,true,false],\"post_proc\":[\"/\",10]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2]},\"lowBat\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",26,0,\"false\",\"true\"]}}}";
 /*R""""(
 {
   "brand":"Inkbird",
@@ -8,7 +8,7 @@ const char* _IBS_P02B_json = "{\"brand\":\"Inkbird\",\"model\":\"Pool Thermomete
   "condition":["name", "index", 0, "IBS-P02B", "&", "manufacturerdata", "=", 36],
   "properties":{
     "tempc":{
-      "decoder":["value_from_hex_data","manufacturerdata", 13, 4, true],
+      "decoder":["value_from_hex_data","manufacturerdata", 12, 2, true, false],
       "post_proc":["/",10]
     },
    "batt":{
