2c1762d852b35ae459efd81d06def349086fb26c
DigiH <17110652+DigiH@users.noreply.github.com>
2025-08-26 15:28:48 +0200

    Add MAC address property to IBS-P02B decoder

--- src/devices/IBS_P02B_json.h
+++ src/devices/IBS_P02B_json.h
@@ -1,4 +1,4 @@
-const char* _IBS_P02B_json = "{\"brand\":\"Inkbird\",\"model\":\"Pool Thermometer\",\"model_id\":\"IBS-P02B\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"IBS-P02B\",\"&\",\"manufacturerdata\",\"=\",36],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",12,2,true,false],\"post_proc\":[\"/\",10]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2]},\"lowbatt\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",26,0,false,true]},\"displayunit\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",23,0,\"°C\",\"°F\"]}}}";
+const char* _IBS_P02B_json = "{\"brand\":\"Inkbird\",\"model\":\"Pool Thermometer\",\"model_id\":\"IBS-P02B\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"IBS-P02B\",\"&\",\"manufacturerdata\",\"=\",36],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",12,2,true,false],\"post_proc\":[\"/\",10]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2]},\"lowbatt\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",26,0,false,true]},\"displayunit\":{\"decoder\":[\"bit_static_value\",\"manufacturerdata\",23,0,\"°C\",\"°F\"]},\"mac\":{\"decoder\":[\"mac_from_hex_data\",\"manufacturerdata\",0]}}}";
 /*R""""(
 {
   "brand":"Inkbird",
@@ -19,11 +19,14 @@ const char* _IBS_P02B_json = "{\"brand\":\"Inkbird\",\"model\":\"Pool Thermomete
    },
    "displayunit":{
       "decoder":["bit_static_value", "manufacturerdata", 23, 0, "°C", "°F"]
+   },
+   "mac":{
+      "decoder":["mac_from_hex_data", "manufacturerdata", 0]
    }
   }
 })"""";*/
 
-const char* _IBS_P02B_json_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"lowbatt\":{\"unit\":\"status\",\"name\":\"battery\"},\"displayunit\":{\"unit\":\"string\",\"name\":\"displayUnit\"}}}";
+const char* _IBS_P02B_json_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"batt\":{\"unit\":\"%\",\"name\":\"battery\"},\"lowbatt\":{\"unit\":\"status\",\"name\":\"battery\"},\"displayunit\":{\"unit\":\"string\",\"name\":\"displayUnit\"},\"mac\":{\"unit\":\"string\",\"name\":\"MAC address\"}}}";
 /*R""""(
 {
    "properties":{
@@ -42,6 +45,10 @@ const char* _IBS_P02B_json_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\"
       "displayunit":{
          "unit":"string",
          "name":"displayUnit"
+      },
+      "mac":{
+         "unit":"string",
+         "name":"MAC address"
       }
    }
 })"""";*/
