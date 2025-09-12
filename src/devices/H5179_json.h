ef36e8574d909440189ff879023026913d85d67b
DigiH <17110652+DigiH@users.noreply.github.com>
2025-09-12 18:10:39 +0200

    Govee H5179 new shorter braodcast name and manufacturerdata (#670)

--- src/devices/H5179_json.h
+++ src/devices/H5179_json.h
@@ -23,4 +23,35 @@ const char* _H5179_json = "{\"brand\":\"Govee\",\"model\":\"Thermo-Hygrometer\",
    }
 })"""";*/
 
+const char* _H5179_json_N = "{\"brand\":\"Govee\",\"model\":\"Thermo-Hygrometer\",\"model_id\":\"H5179_N\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"GV5179\",\"&\",\"manufacturerdata\",\">=\",16,\"index\",0,\"0100\"],\"properties\":{\"tempc\":{\"condition\":[\"manufacturerdata\",8,\"bit\",3,0],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",8,6,false,false],\"post_proc\":[\"/\",1000,\">\",0,\"/\",10]},\"_tempc\":{\"condition\":[\"manufacturerdata\",8,\"bit\",3,1],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",8,6,false,false],\"post_proc\":[\"&\",8388607,\"/\",1000,\">\",0,\"/\",10,\"*\",-1]},\"hum\":{\"condition\":[\"name\",\"not_contain\",\"GV5108\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",8,6,false,false],\"post_proc\":[\"&\",8388607,\"%\",1000,\"/\",10]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",14,2,false,false]}}}";
+
+/* R""""(
+{
+   "brand":"Govee",
+   "model":"Thermo-Hygrometer",
+   "model_id":"H5179_N",
+   "tag":"0103",
+   "condition":["name", "index", 0, "GV5179", "&", "manufacturerdata", ">=", 16, "index", 0, "0100"],
+   "properties":{
+      "tempc":{
+         "condition":["manufacturerdata", 8, "bit", 3, 0],
+         "decoder":["value_from_hex_data", "manufacturerdata", 8, 6, false, false],
+         "post_proc":["/", 1000, ">", 0, "/", 10]
+      },
+      "_tempc":{
+         "condition":["manufacturerdata", 8, "bit", 3, 1],
+         "decoder":["value_from_hex_data", "manufacturerdata", 8, 6, false, false],
+         "post_proc":["&", 8388607, "/", 1000, ">", 0, "/", 10, "*", -1]
+      },
+      "hum":{
+         "condition":["name", "not_contain", "GV5108"],
+         "decoder":["value_from_hex_data", "manufacturerdata", 8, 6, false, false],
+         "post_proc":["&", 8388607, "%", 1000, "/", 10]
+      },
+      "batt":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 14, 2, false, false]
+      }
+   }
+})"""";*/
+
 const char* _H5179_json_props = _common_BTH_props;
