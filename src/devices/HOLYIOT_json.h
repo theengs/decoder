34455cc16c396b21d32b08160d52fd03ca2f814c
kickouille <53976543+kickouille@users.noreply.github.com>
2025-10-13 15:00:48 +0200

    Update HOLYIOT_json.h
    Changed service data index 0 from 416 to 41 as the 6 is part of battery level.

--- src/devices/HOLYIOT_json.h
+++ src/devices/HOLYIOT_json.h
@@ -1,11 +1,11 @@
-const char* _HOLYIOT_json = "{\"brand\":\"HolyIoT\",\"model\":\"Beacon\",\"model_id\":\"HOLYIOT\",\"tag\":\"1009\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c000215\",\"&\",\"servicedata\",\"=\",26,\"index\",0,\"416\",\"&\",\"uuid\",\"index\",0,\"5242\"],\"properties\":{\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",2,2]},\"device\":{\"decoder\":[\"static_value\",\"HolyIoT Beacon Tracker\"]}}}";
+const char* _HOLYIOT_json = "{\"brand\":\"HolyIoT\",\"model\":\"Beacon\",\"model_id\":\"HOLYIOT\",\"tag\":\"1009\",\"condition\":[\"manufacturerdata\",\"=\",50,\"index\",0,\"4c000215\",\"&\",\"servicedata\",\"=\",26,\"index\",0,\"41\",\"&\",\"uuid\",\"index\",0,\"5242\"],\"properties\":{\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",2,2]},\"device\":{\"decoder\":[\"static_value\",\"HolyIoT Beacon Tracker\"]}}}";
 /*R""""(
 {
    "brand":"HolyIoT",
    "model":"Beacon",
    "model_id":"HOLYIOT",
    "tag":"1009",
-   "condition":["manufacturerdata", "=", 50, "index", 0, "4c000215", "&", "servicedata", "=", 26, "index", 0, "416", "&", "uuid", "index", 0, "5242"],
+   "condition":["manufacturerdata", "=", 50, "index", 0, "4c000215", "&", "servicedata", "=", 26, "index", 0, "41", "&", "uuid", "index", 0, "5242"],
    "properties":{
       "batt":{
          "decoder":["value_from_hex_data", "servicedata", 2, 2]
