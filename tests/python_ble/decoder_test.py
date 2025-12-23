14dc3559c202c7c4dc4d601bed4c85597fbf5683
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2025-12-23 19:07:41 -0300

    test: Update python tests to use unittest

--- tests/python_ble/decoder_test.py
+++ tests/python_ble/decoder_test.py
@@ -1,19 +1,19 @@
-from TheengsDecoder import decodeBLE as dble
-from TheengsDecoder import getProperties
-from TheengsDecoder import getAttribute
+import asyncio
 import json
 
-x = {"servicedata":"712098004a63b6658d7cc40d071003f32600"}
-z = dble(json.dumps(x))
-print(z, "\n")
+from TheengsDecoder import decodeBLE as dble
 
-p = json.loads(z)
-print(getProperties(p['model_id']), "\n")
-brand = getAttribute(p['model_id'], 'brand')
-model = getAttribute(p['model_id'], 'model')
-print("brand:", brand, ", model:", model)
+from unittest import TestCase
 
-y = {}
-z = dble(json.dumps(y))
-print("decoder result (None expected): ", z)
-print("Done")
+
+class TestScanAndDecode(TestCase):
+    def test_th05f(self):
+        data = {"servicedatauuid": "fcd2", "servicedata": "4000720154022e0a03071d0c190b", "name": "TH05F-C451CC", "id": "38:1F:8D:C4:51:CC", "rssi": -79}
+        parsed = json.loads(dble(json.dumps(data)))
+        self.assertEqual(parsed.get("model_id"), "TH05F")
+        self.assertEqual(data.get("servicedata"), parsed.get("servicedata"))
+        self.assertEqual(parsed.get('tempc'), 26.06)
+        self.assertEqual(parsed.get('tempf'), 78.908)
+        self.assertEqual(parsed.get('hum'),   74.31)
+        self.assertEqual(parsed.get('batt'),  84)
+        self.assertEqual(parsed.get('volt'),  2.841)
