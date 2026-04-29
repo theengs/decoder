b6fb3ea3967417b9ecf8f7017211176c1e993a15
DigiH <17110652+DigiH@users.noreply.github.com>
2026-04-29 11:34:28 +0200

    SmartSolar MPPT Initial

---
+++ docs/devices/VICTRON_SCC.md
@@ -0,0 +1,13 @@
+# Victron Energy SmartSolar MPPT
+
+|Model Id|[VICTSCC](https://github.com/theengs/decoder/blob/development/src/devices/VICTRON_SCC_json.h)|
+|-|-|
+|Brand|Victron Energy|
+|Model|SmartSolar MPPT|
+|Short Description|Various Solar Charge Controller models|
+|Communication|BLE broadcast|
+|Frequency|2.4Ghz|
+|Power Source|From connected battery|
+|Exchanged Data|device state, voltage battery, current battery, yield today, pv power, current load, error code|
+|Encrypted|Yes|
+|Device Tracker|&#9989;|
--- src/devices/VICTRON__ENCR_json.h
+++ src/devices/VICTRON__ENCR_json.h
@@ -1,21 +1,25 @@
-const char* _VICTRON_ENCR_json = "{\"brand\":\"Victron Energy\",\"model\":\"Victron encrypted\",\"model_id\":\"VICTRON_ENCR\",\"tag\":\"140003\",\"condition\":[\"manufacturerdata\",\">=\",46,\"index\",0,\"e10210\"],\"properties\":{\"cipher\":{\"condition\":[\"manufacturerdata\",\"=\",46],\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,26]},\"_cipher\":{\"condition\":[\"manufacturerdata\",\"=\",48],\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,28]},\"__cipher\":{\"condition\":[\"manufacturerdata\",\"=\",50],\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,30]},\"ctr\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",14,4,true]},\"mic\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",18,2]}}}";
+const char* _VICTRON_ENCR_json = "{\"brand\":\"Victron Energy\",\"model\":\"Victron encrypted\",\"model_id\":\"VICTRON_ENCR\",\"tag\":\"140003\",\"condition\":[\"manufacturerdata\",\">=\",44,\"index\",0,\"e10210\"],\"properties\":{\"cipher\":{\"condition\":[\"manufacturerdata\",\"=\",44],\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,24]},\"_cipher\":{\"condition\":[\"manufacturerdata\",\"=\",46],\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,26]},\"__cipher\":{\"condition\":[\"manufacturerdata\",\"=\",48],\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,28]},\"___cipher\":{\"condition\":[\"manufacturerdata\",\"=\",50],\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,30]},\"ctr\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",14,4,true]},\"mic\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",18,2]}}}";
 /*R""""(
 {
    "brand":"Victron Energy",
    "model":"Victron encrypted",
    "model_id":"VICTRON_ENCR",
    "tag":"140003",
-   "condition":["manufacturerdata", ">=", 46, "index", 0, "e10210"],
+   "condition":["manufacturerdata", ">=", 44, "index", 0, "e10210"],
    "properties":{
       "cipher":{
+         "condition":["manufacturerdata", "=", 44],
+         "decoder":["string_from_hex_data", "manufacturerdata", 20, 24]
+      },
+      "_cipher":{
          "condition":["manufacturerdata", "=", 46],
          "decoder":["string_from_hex_data", "manufacturerdata", 20, 26]
       },
-      "_cipher":{
+      "__cipher":{
          "condition":["manufacturerdata", "=", 48],
          "decoder":["string_from_hex_data", "manufacturerdata", 20, 28]
       },
-      "__cipher":{
+      "___cipher":{
          "condition":["manufacturerdata", "=", 50],
          "decoder":["string_from_hex_data", "manufacturerdata", 20, 30]
       },
