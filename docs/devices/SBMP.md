58f14b8c0417533ad6a950d62bf832f56b8c0102
DigiH <17110652+DigiH@users.noreply.github.com>
2026-07-05 22:21:55 +0200

    SwitchBot Meter Pro without CO2 added (#701)

--- docs/devices/SBMP.md
+++ docs/devices/SBMP.md
@@ -4,9 +4,9 @@
 |-|-|
 |Brand|SwitchBot|
 |Model|Meter Pro (CO2)|
-|Short Description|Thermo-Hygrometer and CO2 Monitor|
+|Short Description|Thermo-Hygrometer with optional CO2 Monitor|
 |Communication|BLE broadcast|
 |Frequency|2.4Ghz|
 |Power Source|2 AA|
-|Exchanged Data|temperature, humidity, carbon dioxide, battery|
+|Exchanged Data|temperature, humidity, battery, carbon dioxide (CO2 version only)|
 |Encrypted|No|
--- src/devices/SBMP_json.h
+++ src/devices/SBMP_json.h
@@ -1,13 +1,13 @@
 #include "common_props.h"
 
-const char* _SBMP_json = "{\"brand\":\"SwitchBot\",\"model\":\"Meter Pro (CO2)\",\"model_id\":\"W490001X\",\"tag\":\"0f02\",\"condition\":[\"uuid\",\"index\",0,\"fd3d\",\"&\",\"servicedata\",\"=\",6,\"index\",0,\"35\",\"&\",\"manufacturerdata\",\"=\",36,\"index\",0,\"6909\"],\"properties\":{\".cal\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",21,1,false,false],\"post_proc\":[\"/\",10]},\"tempc\":{\"condition\":[\"manufacturerdata\",22,\"bit\",3,0],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,2,true,false],\"post_proc\":[\"+\",\".cal\",\"*\",-1]},\"_tempc\":{\"condition\":[\"manufacturerdata\",22,\"bit\",3,1],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,2,true,false],\"post_proc\":[\"+\",\".cal\",\"-\",128]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,2,false,false],\"post_proc\":[\"&\",127]},\"co2\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",30,4,false,false]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false],\"post_proc\":[\"&\",127]},\"mac\":{\"decoder\":[\"mac_from_hex_data\",\"manufacturerdata\",4]}}}";
+const char* _SBMP_json = "{\"brand\":\"SwitchBot\",\"model\":\"Meter Pro (CO2)\",\"model_id\":\"W490001X\",\"tag\":\"0202\",\"condition\":[\"servicedata\",\"=\",6,\"index\",0,\"35\",\"|\",\"servicedata\",\"=\",6,\"index\",0,\"34\",\"&\",\"uuid\",\"index\",0,\"fd3d\",\"&\",\"manufacturerdata\",\">=\",30,\"index\",0,\"6909\"],\"properties\":{\".cal\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",21,1,false,false],\"post_proc\":[\"/\",10]},\"tempc\":{\"condition\":[\"manufacturerdata\",22,\"bit\",3,0],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,2,true,false],\"post_proc\":[\"+\",\".cal\",\"*\",-1]},\"_tempc\":{\"condition\":[\"manufacturerdata\",22,\"bit\",3,1],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,2,true,false],\"post_proc\":[\"+\",\".cal\",\"-\",128]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,2,false,false],\"post_proc\":[\"&\",127]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",4,2,false,false],\"post_proc\":[\"&\",127]},\"mac\":{\"decoder\":[\"mac_from_hex_data\",\"manufacturerdata\",4]},\"co2\":{\"condition\":[\"manufacturerdata\",\"=\",36],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",30,4,false,false]}}}";
 /*R""""(
 {
    "brand":"SwitchBot",
    "model":"Meter Pro (CO2)",
    "model_id":"W490001X",
-   "tag":"0f02",
-   "condition":["uuid", "index", 0, "fd3d", "&", "servicedata", "=", 6, "index", 0, "35", "&", "manufacturerdata", "=", 36, "index", 0, "6909"],
+   "tag":"0202",
+   "condition":["servicedata", "=", 6, "index", 0, "35", "|", "servicedata", "=", 6, "index", 0, "34", "&", "uuid", "index", 0, "fd3d", "&", "manufacturerdata", ">=", 30, "index", 0, "6909"],
    "properties":{
       ".cal":{
          "decoder":["value_from_hex_data", "manufacturerdata", 21, 1, false, false],
@@ -27,15 +27,16 @@ const char* _SBMP_json = "{\"brand\":\"SwitchBot\",\"model\":\"Meter Pro (CO2)\"
          "decoder":["value_from_hex_data", "manufacturerdata", 24, 2, false, false],
          "post_proc":["&", 127]
       },
-      "co2":{
-         "decoder":["value_from_hex_data", "manufacturerdata", 30, 4, false, false]
-      },
       "batt":{
          "decoder":["value_from_hex_data", "servicedata", 4, 2, false, false],
          "post_proc":["&", 127]
       },
       "mac":{
          "decoder":["mac_from_hex_data", "manufacturerdata", 4]
+      },
+      "co2":{
+         "condition":["manufacturerdata", "=", 36],
+         "decoder":["value_from_hex_data", "manufacturerdata", 30, 4, false, false]
       }
    }
 })"""";*/
