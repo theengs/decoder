6d549e47740ec524e80ed11b295d18f89402e43f
DigiH <17110652+DigiH@users.noreply.github.com>
2026-08-28 21:28:49 +0200

    Amended Victron "Battery Monitor/Smart Sense/SmartShunt" decoder

--- src/devices/VICTRON_SBS_json.h
+++ src/devices/VICTRON_SBS_json.h
@@ -1,21 +1,49 @@
-const char* _VICTSBS_json = "{\"brand\":\"Victron Energy\",\"model\":\"Smart Battery Sense\",\"model_id\":\"VICTSBS\",\"tag\":\"0c48\",\"condition\":[\"manufacturerdata\",\"index\",8,\"a5a3\",\"|\",\"manufacturerdata\",\"index\",8,\"a4a3\",\"&\",\"manufacturerdata\",\"=\",50,\"index\",0,\"e10211\",\"&\",\"manufacturerdata\",\"index\",12,\"02ffff\"],\"properties\":{\"volt\":{\"condition\":[\"manufacturerdata\",24,\"!\",\"ffff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,4,true,false],\"post_proc\":[\"/\",100]},\"tempc\":{\"condition\":[\"manufacturerdata\",37,\"bit\",0,0,\"&\",\"manufacturerdata\",37,\"bit\",1,1],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",32,4,true,false],\"post_proc\":[\"-\",27315,\"/\",100]},\"alarm_reason\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",28,4]}}}";
+const char* _VICTSBS_json = "{\"brand\":\"Victron Energy\",\"model\":\"Battery Monitor/Smart Sense/SmartShunt\",\"model_id\":\"VICTSBS\",\"tag\":\"0c48\",\"condition\":[\"manufacturerdata\",\">=\",50,\"index\",0,\"e10211\",\"&\",\"manufacturerdata\",\"index\",12,\"02ffff\"],\"properties\":{\"ttg\":{\"condition\":[\"manufacturerdata\",20,\"!\",\"ffff\"],\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",80,16,false]},\"volt\":{\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",96,16,true],\"post_proc\":[\"/\",100]},\"volt_aux\":{\"condition\":[\"manufacturerdata\",37,\"bit\",0,0,\"&\",\"manufacturerdata\",37,\"bit\",1,0],\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",128,16,true],\"post_proc\":[\"/\",100]},\"volt_mid\":{\"condition\":[\"manufacturerdata\",37,\"bit\",0,1,\"&\",\"manufacturerdata\",37,\"bit\",1,0],\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",128,16,true],\"post_proc\":[\"/\",100]},\"tempc\":{\"condition\":[\"manufacturerdata\",37,\"bit\",0,0,\"&\",\"manufacturerdata\",37,\"bit\",1,1],\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",128,16,true],\"post_proc\":[\"-\",27315,\"/\",100]},\"current\":{\"condition\":[\"manufacturerdata\",41,\"!\",\"fffff\"],\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",146,22,true],\"post_proc\":[\"/\",1000]},\"consumed_ah\":{\"condition\":[\"manufacturerdata\",42,\"!\",\"ffffff\"],\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",168,20,true],\"post_proc\":[\"/\",-10]},\"soc\":{\"condition\":[\"manufacturerdata\",46,\"!\",\"fff\"],\"decoder\":[\"value_from_bit_data\",\"manufacturerdata\",188,10,false],\"post_proc\":[\"/\",10]},\"alarm_reason\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",28,4]}}}";
 /*R""""(
 {
    "brand":"Victron Energy",
-   "model":"Smart Battery Sense",
+   "model":"Battery Monitor/Smart Sense/SmartShunt",
    "model_id":"VICTSBS",
    "tag":"0c48",
-   "condition":["manufacturerdata", "index", 8, "a5a3", "|", "manufacturerdata", "index", 8, "a4a3", "&", "manufacturerdata", "=", 50, "index", 0, "e10211", "&", "manufacturerdata", "index", 12, "02ffff"],
+   "condition":["manufacturerdata", ">=", 50, "index", 0, "e10211", "&", "manufacturerdata", "index", 12, "02ffff"],
    "properties":{
+      "ttg":{
+         "condition":["manufacturerdata", 20, "!", "ffff"],
+         "decoder":["value_from_bit_data", "manufacturerdata", 80, 16, false]
+      },
       "volt":{
-        "condition":["manufacturerdata", 24, "!", "ffff"],
-        "decoder":["value_from_hex_data", "manufacturerdata", 24, 4, true, false],
-        "post_proc":["/", 100]
+         "decoder":["value_from_bit_data", "manufacturerdata", 96, 16, true],
+         "post_proc":["/", 100]
+      },
+      "volt_aux":{
+         "condition":["manufacturerdata", 37, "bit", 0, 0, "&", "manufacturerdata", 37, "bit", 1, 0],
+         "decoder":["value_from_bit_data", "manufacturerdata", 128, 16, true],
+         "post_proc":["/", 100]
+      },
+      "volt_mid":{
+         "condition":["manufacturerdata", 37, "bit", 0, 1, "&", "manufacturerdata", 37, "bit", 1, 0],
+         "decoder":["value_from_bit_data", "manufacturerdata", 128, 16, true],
+         "post_proc":["/", 100]
       },
       "tempc":{
-        "condition":["manufacturerdata", 37, "bit", 0, 0, "&", "manufacturerdata", 37, "bit", 1, 1],
-        "decoder":["value_from_hex_data", "manufacturerdata", 32, 4, true, false],
-        "post_proc":["-", 27315, "/", 100]
+         "condition":["manufacturerdata", 37, "bit", 0, 0, "&", "manufacturerdata", 37, "bit", 1, 1],
+         "decoder":["value_from_bit_data", "manufacturerdata", 128, 16, true],
+         "post_proc":["-", 27315, "/", 100]
+      },
+      "current":{
+         "condition":["manufacturerdata", 41, "!", "fffff"],
+         "decoder":["value_from_bit_data", "manufacturerdata", 146, 22, true],
+         "post_proc":["/", 1000]
+      },
+      "consumed_ah":{
+         "condition":["manufacturerdata", 42, "!", "ffffff"],
+         "decoder":["value_from_bit_data", "manufacturerdata", 168, 20, true],
+         "post_proc":["/", -10]
+      },
+      "soc":{
+         "condition":["manufacturerdata", 46, "!", "fff"],
+         "decoder":["value_from_bit_data", "manufacturerdata", 188, 10, false],
+         "post_proc":["/", 10]
       },
       "alarm_reason":{
         "decoder":["value_from_hex_data", "manufacturerdata", 28, 4]
@@ -23,17 +51,41 @@ const char* _VICTSBS_json = "{\"brand\":\"Victron Energy\",\"model\":\"Smart Bat
    }
 })"""";*/
 
-const char* _VICTSBS_json_props = "{\"properties\":{\"volt\":{\"unit\":\"V\",\"name\":\"voltage\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"alarm_reason\":{\"unit\":\"int\",\"name\":\"alarm reason\"}}}";
+const char* _VICTSBS_json_props = "{\"properties\":{\"ttg\":{\"unit\":\"min\",\"name\":\"duration\"},\"volt\":{\"unit\":\"V\",\"name\":\"voltage\"},\"volt_aux\":{\"unit\":\"V\",\"name\":\"voltage\"},\"volt_mid\":{\"unit\":\"V\",\"name\":\"voltage\"},\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"current\":{\"unit\":\"A\",\"name\":\"current\"},\"consumed_ah\":{\"unit\":\"Ah\",\"name\":\"consumed ampere hours\"},\"soc\":{\"unit\":\"%\",\"name\":\"state of charge\"},\"alarm_reason\":{\"unit\":\"int\",\"name\":\"alarm reason\"}}}";
 /*R""""(
 {
    "properties":{
+      "ttg":{
+         "unit":"min",
+         "name":"duration"
+      },
       "volt": {
          "unit": "V",
          "name": "voltage"
       },
+      "volt_aux":{
+         "unit":"V",
+         "name":"voltage"
+      },
+      "volt_mid":{
+         "unit":"V",
+         "name":"voltage"
+      },
       "tempc":{
          "unit":"°C",
          "name":"temperature"
+      },
+      "current":{
+         "unit":"A",
+         "name":"current"
+      },
+      "consumed_ah":{
+         "unit":"Ah",
+         "name":"consumed ampere hours"
+      },
+      "soc":{
+         "unit":"%",
+         "name":"state of charge"
       },
       "alarm_reason":{
          "unit":"int",
