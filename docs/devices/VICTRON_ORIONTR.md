8fa9dcfed7cdf2759cbfd5a7b08c4e277c5db7f7
Lutz Bender <lutz@lutz-bender.de>
2026-09-04 12:32:12 +0200

    fix device description

--- docs/devices/VICTRON_ORIONTR.md
+++ docs/devices/VICTRON_ORIONTR.md
@@ -8,6 +8,6 @@
 |Communication|BLE broadcast|
 |Frequency|2.4Ghz|
 |Power Source|From connected battery/alternator|
-|Exchanged Data|device state, voltage in, voltage out, current in, current out, error code|
+|Exchanged Data|device state, voltage in, voltage out, off reason, error code|
 |Encrypted|Yes|
 |Device Tracker|&#9989;|
--- src/devices/VICTRON_ORIONTR_json.h
+++ src/devices/VICTRON_ORIONTR_json.h
@@ -1,4 +1,4 @@
-const char* _VICTORIONTR_json = "{\"brand\":\"Victron Energy\",\"model\":\"Orion TR\",\"model_id\":\"VICTORIONTR\",\"tag\":\"0c48\",\"condition\":[\"manufacturerdata\",\"=\",40,\"index\",0,\"e10211\",\"&\",\"manufacturerdata\",\"index\",8,\"d0a3\",\"&\",\"manufacturerdata\",\"index\",12,\"04ffff\"],\"properties\":{\"device_state\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,2],\"lookup\":[\"00\",\"off\",\"01\",\"low power\",\"02\",\"fault\",\"03\",\"bulk\",\"04\",\"absorption\",\"05\",\"float\",\"06\",\"storage\",\"07\",\"equalize manual\",\"09\",\"inverting\",\"0b\",\"power_supply\",\"f5\",\"starting up\",\"f6\",\"repeated absorption\",\"f7\",\"recondition\",\"f8\",\"battery safe\",\"f9\",\"active\",\"fc\",\"external control\",\"ff\",\"N/A\"]},\"volt_in\":{\"condition\":[\"manufacturerdata\",24,\"!\",\"ff7f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,4,true,true],\"post_proc\":[\"/\",100]},\"current_out\":{\"condition\":[\"manufacturerdata\",28,\"!\",\"ff7f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",28,4,true,true],\"post_proc\":[\"/\",10]},\"volt_out\":{\"condition\":[\"manufacturerdata\",32,\"!\",\"ffff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",32,4,true,false],\"post_proc\":[\"/\",100]},\"current_in\":{\"condition\":[\"manufacturerdata\",36,\"!\",\"ffff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",36,4,true,false],\"post_proc\":[\"/\",10]},\"error_code\":{\"condition\":[\"manufacturerdata\",22,\"!\",\"ff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,2]}}}";
+const char* _VICTORIONTR_json = "{\"brand\":\"Victron Energy\",\"model\":\"Orion TR\",\"model_id\":\"VICTORIONTR\",\"tag\":\"0c48\",\"condition\":[\"manufacturerdata\",\"=\",40,\"index\",0,\"e10211\",\"&\",\"manufacturerdata\",\"index\",8,\"d0a3\",\"&\",\"manufacturerdata\",\"index\",12,\"04ffff\"],\"properties\":{\"device_state\":{\"decoder\":[\"string_from_hex_data\",\"manufacturerdata\",20,2],\"lookup\":[\"00\",\"off\",\"01\",\"low power\",\"02\",\"fault\",\"03\",\"bulk\",\"04\",\"absorption\",\"05\",\"float\",\"06\",\"storage\",\"07\",\"equalize manual\",\"09\",\"inverting\",\"0b\",\"power_supply\",\"f5\",\"starting up\",\"f6\",\"repeated absorption\",\"f7\",\"recondition\",\"f8\",\"battery safe\",\"f9\",\"active\",\"fc\",\"external control\",\"ff\",\"N/A\"]},\"volt_in\":{\"condition\":[\"manufacturerdata\",24,\"!\",\"ffff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",24,4,true,false],\"post_proc\":[\"/\",100]},\"volt_out\":{\"condition\":[\"manufacturerdata\",28,\"!\",\"ff7f\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",28,4,true,true],\"post_proc\":[\"/\",100]},\"off_reason\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",32,8,true,false]},\"error_code\":{\"condition\":[\"manufacturerdata\",22,\"!\",\"ff\"],\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",22,2]}}}";
 /*R""""(
 {
    "brand":"Victron Energy",
@@ -28,24 +28,17 @@ const char* _VICTORIONTR_json = "{\"brand\":\"Victron Energy\",\"model\":\"Orion
                    "ff", "N/A"]
       },
       "volt_in":{
-         "condition":["manufacturerdata", 24, "!", "ff7f"],
-         "decoder":["value_from_hex_data", "manufacturerdata", 24, 4, true, true],
+         "condition":["manufacturerdata", 24, "!", "ffff"],
+         "decoder":["value_from_hex_data", "manufacturerdata", 24, 4, true, false],
          "post_proc":["/", 100]
-      },
-      "current_out":{
-         "condition":["manufacturerdata", 28, "!", "ff7f"],
-         "decoder":["value_from_hex_data", "manufacturerdata", 28, 4, true, true],
-         "post_proc":["/", 10]
       },
       "volt_out":{
-         "condition":["manufacturerdata", 32, "!", "ffff"],
-         "decoder":["value_from_hex_data", "manufacturerdata", 32, 4, true, false],
+         "condition":["manufacturerdata", 28, "!", "ff7f"],
+         "decoder":["value_from_hex_data", "manufacturerdata", 28, 4, true, true],
          "post_proc":["/", 100]
       },
-      "current_in":{
-         "condition":["manufacturerdata", 36, "!", "ffff"],
-         "decoder":["value_from_hex_data", "manufacturerdata", 36, 4, true, false],
-         "post_proc":["/", 10]
+      "off_reason":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 32, 8, true, false]
       },
       "error_code":{
          "condition":["manufacturerdata", 22, "!", "ff"],
@@ -54,7 +47,7 @@ const char* _VICTORIONTR_json = "{\"brand\":\"Victron Energy\",\"model\":\"Orion
    }
 })"""";*/
 
-const char* _VICTORIONTR_json_props = "{\"properties\":{\"device_state\":{\"unit\":\"string\",\"name\":\"device state\"},\"volt_out\":{\"unit\":\"V\",\"name\":\"voltage\"},\"current_out\":{\"unit\":\"A\",\"name\":\"current\"},\"volt_in\":{\"unit\":\"V\",\"name\":\"voltage\"},\"current_in\":{\"unit\":\"A\",\"name\":\"current\"},\"error_code\":{\"unit\":\"int\",\"name\":\"error code\"}}}";
+const char* _VICTORIONTR_json_props = "{\"properties\":{\"device_state\":{\"unit\":\"string\",\"name\":\"device state\"},\"volt_out\":{\"unit\":\"V\",\"name\":\"voltage\"},\"volt_in\":{\"unit\":\"V\",\"name\":\"voltage\"},\"off_reason\":{\"unit\":\"int\",\"name\":\"off reason\"},\"error_code\":{\"unit\":\"int\",\"name\":\"error code\"}}}";
 /*R""""(
 {
    "properties":{
@@ -66,17 +59,13 @@ const char* _VICTORIONTR_json_props = "{\"properties\":{\"device_state\":{\"unit
          "unit": "V",
          "name": "voltage"
       },
-      "current_out": {
-         "unit": "A",
-         "name": "current"
-      },
       "volt_in": {
          "unit": "V",
          "name": "voltage"
       },
-      "current_in": {
-         "unit": "A",
-         "name": "current"
+      "off_reason":{
+         "unit":"int",
+         "name":"off reason"
       },
       "error_code":{
          "unit":"int",
